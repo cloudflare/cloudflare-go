89a2e40b0ed76e475a819540d3bd8517ffb5fb34
Salvatore D'Agostino <salvatore@cloudflare.com>
2016-05-06 23:34:16 -0700

    Add new structs for different origin cert resources
    list and revoke send back different types in Result

--- origin_ca.go
+++ origin_ca.go
@@ -19,12 +19,34 @@ type OriginCA struct {
 	Csr             string   `json:"csr"`
 }
 
+// OriginCertificateID is the resource sent back for a revoke action
+type OriginCertificateID struct {
+	ID string `json:"id"`
+}
+
 // OriginCAResponse is the APIv4 response envelop containing the OriginCA result
 type OriginCAResponse struct {
 	Success  bool     `json:"success"`
 	Errors   []string `json:"errors"`
 	Messages []string `json:"messages"`
 	Result   OriginCA `json:"result"`
+}
+
+// OriginCAResponseList is the APIv4 response envelop containing a listof OriginCA result
+type OriginCAResponseList struct {
+	Success    bool       `json:"success"`
+	Errors     []string   `json:"errors"`
+	Messages   []string   `json:"messages"`
+	Result     []OriginCA `json:"result"`
+	ResultInfo ResultInfo `json:"result_info"`
+}
+
+// OriginCAResponseRevoke is the APIv4 response envelop containing a revoked cert id
+type OriginCAResponseRevoke struct {
+	Success  bool                `json:"success"`
+	Errors   []string            `json:"errors"`
+	Messages []string            `json:"messages"`
+	Result   OriginCertificateID `json:"result"`
 }
 
 // CreateOriginCertificate will create an origin certificate for a User
