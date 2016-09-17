01132d430d706116fdcc16540a69f8f905eb6c0d
James O'Gorman <jamesog@cloudflare.com>
2016-09-17 10:59:22 +0000

    Move DNS types to dns.go
    Tidying up - move structs from cloudflare.go into the relevant files.
    
    Addresses #49.

--- cloudflare.go
+++ cloudflare.go
@@ -198,37 +198,6 @@ type Owner struct {
 	OwnerType string `json:"owner_type"`
 }
 
-// DNSRecord represents a DNS record in a zone.
-type DNSRecord struct {
-	ID         string      `json:"id,omitempty"`
-	Type       string      `json:"type,omitempty"`
-	Name       string      `json:"name,omitempty"`
-	Content    string      `json:"content,omitempty"`
-	Proxiable  bool        `json:"proxiable,omitempty"`
-	Proxied    bool        `json:"proxied,omitempty"`
-	TTL        int         `json:"ttl,omitempty"`
-	Locked     bool        `json:"locked,omitempty"`
-	ZoneID     string      `json:"zone_id,omitempty"`
-	ZoneName   string      `json:"zone_name,omitempty"`
-	CreatedOn  time.Time   `json:"created_on,omitempty"`
-	ModifiedOn time.Time   `json:"modified_on,omitempty"`
-	Data       interface{} `json:"data,omitempty"` // data returned by: SRV, LOC
-	Meta       interface{} `json:"meta,omitempty"`
-	Priority   int         `json:"priority,omitempty"`
-}
-
-// DNSRecordResponse represents the response from the DNS endpoint.
-type DNSRecordResponse struct {
-	Response
-	Result DNSRecord `json:"result"`
-}
-
-// DNSListResponse represents the response from the list DNS records endpoint.
-type DNSListResponse struct {
-	Response
-	Result []DNSRecord `json:"result"`
-}
-
 // KeylessSSL represents Keyless SSL configuration.
 type KeylessSSL struct {
 	ID          string    `json:"id"`
