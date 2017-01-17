ff39298804fa485ed1b8480053e82695cd45326c
Matt Silverlock <matt@eatsleeprepeat.net>
2017-01-17 09:00:11 -0800

    [wip] Add Load Balancing types, stubs.

---
+++ loadbalancing/lb.go
@@ -0,0 +1,91 @@
+// Package lb provides an interface for configuring Cloudflare's Load Balancing
+// API endpoints: https://www.cloudflare.com/load-balancing/
+package lb
+
+import cloudflare "github.com/cloudflare/cloudflare-go"
+
+// Client ...
+type Client struct{ apiClient *cloudflare.API }
+
+// NewClient ...
+func NewClient(api *cloudflare.API, organizationID string) (*Client, error) {
+	return nil, nil
+}
+
+// GetLoadBalancers returns a list of the currently configured Load Balancers for the given zone ID.
+// API reference:
+// 	https://api.cloudflare.com/#load-balancing-#get-load-balancer
+//	GET /zone/:zone_id/load_balancers
+func (c *Client) GetLoadBalancers(zoneID string) ([]LB, error) {
+
+	return LBListResponse{}, nil
+}
+
+// GetLoadBalancerByID returns a single Load Balancer by its ID.
+func (c *Client) GetLoadBalancerByID(id string) (LB, error) {
+
+	return LBResponse{}, nil
+}
+
+// CreateLoadBalancer creates a Load Balancer.
+func (c *Client) CreateLoadBalancer(lb LB) (LB, error) {
+
+	return LB, nil
+}
+
+// UpdateLoadBalancer updates an existing Load Balancer object. The existing Load Balancer is
+// replaced, not patched.
+func (c *Client) UpdateLoadBalancer(lb LB) error {
+
+	return LBResponse{}, nil
+}
+
+// GetPools returns a list of the currently configured Pools.
+func (c *Client) GetPools() ([]Pool, error) {
+
+	return PoolListResponse{}, nil
+}
+
+// GetPoolByID returns a single Pool by its ID.
+func (c *Client) GetPoolByID(id string) (Pool, error) {
+
+	return PoolResponse, nil
+}
+
+// CreatePool creates a Pool.
+func (c *Client) CreatePool(pool Pool) error {
+
+	return PoolResponse, nil
+}
+
+// UpdatePool updates an existing Pool object. The existing Pool is replaced,
+// not patched.
+func (c *Client) UpdatePool(pool Pool) error {
+
+	return PoolResponse, nil
+}
+
+// GetMonitors returns a list of the currently configured Monitors.
+func (c *Client) GetMonitors() (MonitorListResponse, error) {
+
+	return MonitorListResponse{}, nil
+}
+
+// GetMonitorByID returns a single Monitor by its ID.
+func (c *Client) GetMonitorByID(id string) (MonitorResponse, error) {
+
+	return MonitorResponse, nil
+}
+
+// CreateMonitor creates a Monitor.
+func (c *Client) CreateMonitor(monitor Monitor) (MonitorResponse, error) {
+
+	return MonitorResponse, nil
+}
+
+// UpdateMonitor updates an existing Monitor object. The existing Monitor is
+// replaced, not patched.
+func (c *Client) UpdateMonitor(monitor Monitor) (MonitorResponse, error) {
+
+	return MonitorResponse, nil
+}
---
+++ loadbalancing/lb_types.go
@@ -0,0 +1,90 @@
+package lb
+
+import (
+	"net/http"
+	"time"
+)
+
+// Monitor represents a 'health check' configuration. A Monitor will only
+// actively health check origin servers once applied to a Pool.
+type Monitor struct {
+	CreatedOn       time.Time   `json:"created_on"`
+	Description     string      `json:"description"`
+	ExpectedBody    string      `json:"expected_body"`
+	ExpectedCodes   string      `json:"expected_codes"`
+	FollowRedirects bool        `json:"follow_redirects"`
+	Header          http.Header `json:"header"`
+	ID              string      `json:"id"`
+	Interval        int         `json:"interval"`
+	Method          string      `json:"method"`
+	ModifiedOn      time.Time   `json:"modified_on"`
+	Path            string      `json:"path"`
+	Retries         int         `json:"retries"`
+	Timeout         int         `json:"timeout"`
+	Type            string      `json:"type"`
+}
+
+// MonitorResponse represents a response from the Monitors endpoint.
+type MonitorResponse struct {
+	Result []Monitor `json:"result"`
+	Response
+	ResultInfo `json:"result_info"`
+}
+
+// MonitorListResponse repsents a list response from the Monitors endpoint.
+type MonitorListResponse struct {
+	Result []Monitor `json:"result"`
+	Response
+	ResultInfo `json:"result_info"`
+}
+
+// Pool represents a 'pool' (group) of origin servers, each identified by their
+// IP address or hostname. You can configure multiple Pools, and configure a
+// failover priority (Pool A -> Pool B -> Pool C) as needed. If you're familiar
+// with DNS terminology, think of a Pool as a DNS 'record set' that only
+// returns healthy addresses.
+type Pool struct {
+	CreatedOn         time.Time `json:"created_on"`
+	Description       string    `json:"description"`
+	Enabled           bool      `json:"enabled"`
+	ID                string    `json:"id"`
+	MinimumOrigins    int       `json:"minimum_origins"`
+	ModifiedOn        time.Time `json:"modified_on"`
+	Monitor           string    `json:"monitor"`
+	Name              string    `json:"name"`
+	NotificationEmail string    `json:"notification_email"`
+	Origins           []struct {
+		Name    string `json:"name"`
+		Address string `json:"address"`
+		Enabled bool   `json:"enabled"`
+	} `json:"origins"`
+}
+
+// PoolResponse represents a response from the Pools endpoint.
+type PoolResponse struct{}
+
+// PoolListResponse represents a list response from the Pools endpoint.
+type PoolListResponse struct{}
+
+// LB represents a Load Balancer: a DNS hostname and the load-balancing/failover/geo-routing policy for the associated Pools.
+type LB struct {
+	CreatedOn    time.Time `json:"created_on"`
+	DefaultPools []string  `json:"default_pools"`
+	Description  string    `json:"description"`
+	FallbackPool string    `json:"fallback_pool"`
+	ID           string    `json:"id"`
+	ModifiedOn   time.Time `json:"modified_on"`
+	Name         string    `json:"name"`
+	// PopPools are an Enterprise-only feature for mapping specific Cloudflare
+	// data centers to pools of origin servers.
+	POPPools    map[string][]string `json:"pop_pools"`
+	Proxied     bool                `json:"proxied"`
+	RegionPools map[string][]string `json:"region_pools"`
+	TTL         int                 `json:"ttl"`
+}
+
+// LBResponse represents a list response from the Load Balancers endpoint.
+type LBResponse struct{}
+
+// LBListResponse represents a list response from the Load Balancers endpoint.
+type LBListResponse struct{}
