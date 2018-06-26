ad5edcba2b7318e00c0ae83fa759fb6ddd0b44c3
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-06-26 10:32:49 +1000

    Add `Account`[1] endpoint
    Similar to the `ListAccounts` endpoint, this updates the library to
    fetch a single Account based on the ID.
    
    [1]: https://api.cloudflare.com/#accounts-account-details

--- accounts.go
+++ accounts.go
@@ -20,6 +20,14 @@ type Account struct {
 	Settings AccountSettings `json:"settings"`
 }
 
+// AccountResponse represents the response from the accounts endpoint for a
+// single account ID.
+type AccountResponse struct {
+	Result Account `json:"result"`
+	Response
+	ResultInfo `json:"result_info"`
+}
+
 // AccountListResponse represents the response from the list accounts endpoint.
 type AccountListResponse struct {
 	Result []Account `json:"result"`
@@ -55,4 +63,24 @@ func (api *API) ListAccounts(pageOpts PaginationOptions) ([]Account, ResultInfo,
 		return []Account{}, ResultInfo{}, errors.Wrap(err, errUnmarshalError)
 	}
 	return accListResponse.Result, accListResponse.ResultInfo, nil
+}
+
+// Account returns a single account based on the ID.Account
+//
+// API reference: https://api.cloudflare.com/#accounts-account-details
+func (api *API) Account(accountID string) (Account, ResultInfo, error) {
+	uri := "/accounts/" + accountID
+
+	res, err := api.makeRequest("GET", uri, nil)
+	if err != nil {
+		return Account{}, ResultInfo{}, errors.Wrap(err, errMakeRequestError)
+	}
+
+	var accResponse AccountResponse
+	err = json.Unmarshal(res, &accResponse)
+	if err != nil {
+		return Account{}, ResultInfo{}, errors.Wrap(err, errUnmarshalError)
+	}
+
+	return accResponse.Result, accResponse.ResultInfo, nil
 }
--- accounts_test.go
+++ accounts_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var expectedListAccountsStruct = Account{
+var expectedAccountStruct = Account{
 	ID:   "01a7362d577a6c3019a474fd6f485823",
 	Name: "Cloudflare Demo",
 	Settings: AccountSettings{
@@ -47,9 +47,47 @@ func TestListAccounts(t *testing.T) {
 	}
 
 	mux.HandleFunc("/accounts", handler)
-	want := []Account{expectedListAccountsStruct}
+	want := []Account{expectedAccountStruct}
 
 	actual, _, err := client.ListAccounts(PaginationOptions{})
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestAccount(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": {
+				"id": "01a7362d577a6c3019a474fd6f485823",
+				"name": "Cloudflare Demo",
+				"settings": {
+					"enforce_twofactor": false
+				}
+			},
+			"result_info": {
+				"page": 1,
+				"per_page": 20,
+				"count": 1,
+				"total_count": 2000
+			}
+		}
+		`)
+	}
+
+	mux.HandleFunc("/accounts/01a7362d577a6c3019a474fd6f485823", handler)
+	want := expectedAccountStruct
+
+	actual, _, err := client.Account("01a7362d577a6c3019a474fd6f485823")
 
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
