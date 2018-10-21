39b04e8bbd4450db8c747807eb7bfa781111b727
Jacob Bednarz <jacob.bednarz@gmail.com>
2018-10-22 09:16:43 +1100

    Add support for fetching custom pages
    This introduces the ability to fetch the zone or account level custom
    pages based on either the `ZoneID` or `AccountID` respectively.
    
    To control which to fetch, you need to provide `CustomPageOptions` which
    either takes a `ZoneID` or `AccountID` value.
    
    Zone API reference: https://api.cloudflare.com/#custom-pages-for-a-zone-list-available-custom-pages
    Account API reference: https://api.cloudflare.com/#custom-pages-account--list-custom-pages

--- custom_pages.go
+++ custom_pages.go
@@ -1,16 +1,23 @@
 package cloudflare
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/pkg/errors"
+)
 
 // CustomPage represents a custom page configuration.
 type CustomPage struct {
-	CreatedOn      string    `json:"created_on"`
+	CreatedOn      time.Time `json:"created_on"`
 	ModifiedOn     time.Time `json:"modified_on"`
 	URL            string    `json:"url"`
 	State          string    `json:"state"`
 	RequiredTokens []string  `json:"required_tokens"`
 	PreviewTarget  string    `json:"preview_target"`
 	Description    string    `json:"description"`
+	ID             string    `json:"id"`
 }
 
 // CustomPageResponse represents the response from the custom pages endpoint.
@@ -19,11 +26,54 @@ type CustomPageResponse struct {
 	Result []CustomPage `json:"result"`
 }
 
-// https://api.cloudflare.com/#custom-pages-for-a-zone-available-custom-pages
-// GET /zones/:zone_identifier/custom_pages
+// CustomPageOptions is used to determine whether or not the operation
+// should take place on an account or zone level based on which is
+// provided to the function.
+//
+// A non-empty value denotes desired use.
+type CustomPageOptions struct {
+	AccountID string
+	ZoneID    string
+}
 
-// https://api.cloudflare.com/#custom-pages-for-a-zone-custom-page-details
-// GET /zones/:zone_identifier/custom_pages/:identifier
+// CustomPages lists custom pages for a zone or account.
+//
+// Zone API reference: https://api.cloudflare.com/#custom-pages-for-a-zone-list-available-custom-pages
+// Account API reference: https://api.cloudflare.com/#custom-pages-account--list-custom-pages
+func (api *API) CustomPages(options *CustomPageOptions) ([]CustomPage, error) {
+	var (
+		pageType, identifier string
+	)
 
-// https://api.cloudflare.com/#custom-pages-for-a-zone-update-custom-page-url
-// PUT /zones/:zone_identifier/custom_pages/:identifier
+	if options.AccountID == "" && options.ZoneID == "" {
+		return nil, errors.New("either account ID or zone ID must be provided")
+	}
+
+	if options.AccountID != "" && options.ZoneID != "" {
+		return nil, errors.New("account ID and zone ID are mutually exclusive")
+	}
+
+	// Should the account ID be defined, treat this as an account level operation.
+	if options.AccountID != "" {
+		pageType = "accounts"
+		identifier = options.AccountID
+	} else {
+		pageType = "zones"
+		identifier = options.ZoneID
+	}
+
+	uri := fmt.Sprintf("/%s/%s/custom_pages", pageType, identifier)
+
+	res, err := api.makeRequest("GET", uri, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, errMakeRequestError)
+	}
+
+	var customPageResponse CustomPageResponse
+	err = json.Unmarshal(res, &customPageResponse)
+	if err != nil {
+		return nil, errors.Wrap(err, errUnmarshalError)
+	}
+
+	return customPageResponse.Result, nil
+}
---
+++ custom_pages_test.go
@@ -0,0 +1,124 @@
+package cloudflare
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var timestamp, _ = time.Parse(time.RFC3339Nano, "2014-01-01T05:20:00.12345Z")
+var expectedCustomPage = CustomPage{
+	ID:             "basic_challenge",
+	CreatedOn:      timestamp,
+	ModifiedOn:     timestamp,
+	URL:            "http://www.example.com",
+	State:          "default",
+	RequiredTokens: []string{"::CAPTCHA_BOX::"},
+	PreviewTarget:  "preview:target",
+	Description:    "Basic challenge",
+}
+
+func TestCustomPagesWithoutZoneIDOrAccountID(t *testing.T) {
+	_, err := client.CustomPages(&CustomPageOptions{})
+	assert.EqualError(t, err, "either account ID or zone ID must be provided")
+}
+
+func TestCustomPagesWithZoneIDAndAccountID(t *testing.T) {
+	_, err := client.CustomPages(&CustomPageOptions{ZoneID: "abc123", AccountID: "321cba"})
+	assert.EqualError(t, err, "account ID and zone ID are mutually exclusive")
+}
+
+func TestCustomPagesForZone(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `
+		{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": [
+				{
+					"id": "basic_challenge",
+					"created_on": "2014-01-01T05:20:00.12345Z",
+					"modified_on": "2014-01-01T05:20:00.12345Z",
+					"url": "http://www.example.com",
+					"state": "default",
+					"required_tokens": [
+						"::CAPTCHA_BOX::"
+					],
+					"preview_target": "preview:target",
+					"description": "Basic challenge"
+				}
+			],
+			"result_info": {
+				"page": 1,
+				"per_page": 20,
+				"count": 1,
+				"total_count": 2000
+			}
+		}
+		`)
+	}
+
+	mux.HandleFunc("/zones/d992d6de698eaf2d8cf8fd53b89b18a4/custom_pages", handler)
+	want := []CustomPage{expectedCustomPage}
+
+	pages, err := client.CustomPages(&CustomPageOptions{ZoneID: "d992d6de698eaf2d8cf8fd53b89b18a4"})
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, pages)
+	}
+}
+
+func TestCustomPagesForAccount(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `
+		{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": [
+				{
+					"id": "basic_challenge",
+					"created_on": "2014-01-01T05:20:00.12345Z",
+					"modified_on": "2014-01-01T05:20:00.12345Z",
+					"url": "http://www.example.com",
+					"state": "default",
+					"required_tokens": [
+						"::CAPTCHA_BOX::"
+					],
+					"preview_target": "preview:target",
+					"description": "Basic challenge"
+				}
+			],
+			"result_info": {
+				"page": 1,
+				"per_page": 20,
+				"count": 1,
+				"total_count": 2000
+			}
+		}
+		`)
+	}
+
+	mux.HandleFunc("/accounts/01a7362d577a6c3019a474fd6f485823/custom_pages", handler)
+	want := []CustomPage{expectedCustomPage}
+
+	pages, err := client.CustomPages(&CustomPageOptions{AccountID: "01a7362d577a6c3019a474fd6f485823"})
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, pages)
+	}
+}
