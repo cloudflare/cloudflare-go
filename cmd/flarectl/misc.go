129166671c6ecb20c071c24004aa98f9aa0cdb8a
Rafal Pieniazek <rafal@pieniazek.nl>
2018-11-14 03:41:15 +0100

    flarectl : Add switches to control output of IP sections (#166)
    * adds flag for ip type
    
    * moves methods to utils from main
    
    * adds custom IP display function

--- cmd/flarectl/misc.go
+++ cmd/flarectl/misc.go
@@ -2,22 +2,90 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/codegangsta/cli"
+	"github.com/olekukonko/tablewriter"
+	"github.com/pkg/errors"
 )
 
-func ips(*cli.Context) {
-	ips, _ := cloudflare.IPs()
-	fmt.Println("IPv4 ranges:")
-	for _, r := range ips.IPv4CIDRs {
-		fmt.Println(" ", r)
+// writeTable outputs tabular data to stdout.
+func writeTable(data [][]string, cols ...string) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(cols)
+	table.SetBorder(false)
+	table.AppendBulk(data)
+
+	table.Render()
+}
+
+func checkEnv() error {
+	if api == nil {
+		var err error
+		api, err = cloudflare.New(os.Getenv("CF_API_KEY"), os.Getenv("CF_API_EMAIL"))
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
-	fmt.Println()
-	fmt.Println("IPv6 ranges:")
-	for _, r := range ips.IPv6CIDRs {
-		fmt.Println(" ", r)
+
+	if api.APIKey == "" {
+		return errors.New("API key not defined")
+	}
+	if api.APIEmail == "" {
+		return errors.New("API email not defined")
+	}
+
+	return nil
+}
+
+// Utility function to check if CLI flags were given.
+func checkFlags(c *cli.Context, flags ...string) error {
+	for _, flag := range flags {
+		if c.String(flag) == "" {
+			cli.ShowSubcommandHelp(c)
+			err := errors.Errorf("error: the required flag %q was empty or not provided", flag)
+			fmt.Fprintln(os.Stderr, err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func ips(c *cli.Context) {
+
+	if c.String("ip-type") == "all" {
+		_getIps("ipv4", c.Bool("ip-only"))
+		_getIps("ipv6", c.Bool("ip-only"))
+	} else {
+		_getIps(c.String("ip-type"), c.Bool("ip-only"))
+	}
+}
+
+//
+// gets type of IPs to retrieve and returns results
+//
+func _getIps(ipType string, showMsgType bool) {
+	ips, _ := cloudflare.IPs()
+
+	switch ipType {
+	case "ipv4":
+		if showMsgType != true {
+			fmt.Println("IPv4 ranges:")
+		}
+		for _, r := range ips.IPv4CIDRs {
+			fmt.Println(" ", r)
+		}
+	case "ipv6":
+		if showMsgType != true {
+			fmt.Println("IPv6 ranges:")
+		}
+		for _, r := range ips.IPv6CIDRs {
+			fmt.Println(" ", r)
+		}
 	}
 }
 
