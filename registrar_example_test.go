8b5f27f128bab0236cee234ad59f8c1761194a37
Jacob Bednarz <jacob.bednarz@gmail.com>
2019-03-25 10:41:30 +1100

    registrar: Add support for listing a single domain

---
+++ registrar_example_test.go
@@ -0,0 +1,62 @@
+package cloudflare_test
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	cloudflare "github.com/cloudflare/cloudflare-go"
+)
+
+var (
+	createdAndModifiedTimestamp, _ = time.Parse(time.RFC3339, "2018-08-28T17:26:26Z")
+	expiresAtTimestamp, _          = time.Parse(time.RFC3339, "2019-08-28T23:59:59Z")
+	expectedRegistrarTransferIn    = cloudflare.RegistrarTransferIn{
+		UnlockDomain:      "ok",
+		DisablePrivacy:    "ok",
+		EnterAuthCode:     "needed",
+		ApproveTransfer:   "unknown",
+		AcceptFoa:         "needed",
+		CanCancelTransfer: true,
+	}
+	expectedRegistrarContact = cloudflare.RegistrantContact{
+		ID:           "ea95132c15732412d22c1476fa83f27a",
+		FirstName:    "John",
+		LastName:     "Appleseed",
+		Organization: "Cloudflare, Inc.",
+		Address:      "123 Sesame St.",
+		Address2:     "Suite 430",
+		City:         "Austin",
+		State:        "TX",
+		Zip:          "12345",
+		Country:      "US",
+		Phone:        "+1 123-123-1234",
+		Email:        "user@example.com",
+		Fax:          "123-867-5309",
+	}
+	expectedRegistrarDomain = cloudflare.RegistrarDomain{
+		ID:                "ea95132c15732412d22c1476fa83f27a",
+		Available:         false,
+		SupportedTLD:      true,
+		CanRegister:       false,
+		TransferIn:        expectedRegistrarTransferIn,
+		CurrentRegistrar:  "Cloudflare",
+		ExpiresAt:         expiresAtTimestamp,
+		RegistryStatuses:  "ok,serverTransferProhibited",
+		Locked:            false,
+		CreatedAt:         createdAndModifiedTimestamp,
+		UpdatedAt:         createdAndModifiedTimestamp,
+		RegistrantContact: expectedRegistrarContact,
+	}
+)
+
+func ExampleAPI_RegistrarDomain() {
+	api, err := cloudflare.New(apiKey, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	domain, err := api.RegistrarDomain("01a7362d577a6c3019a474fd6f485823", "cloudflare.com")
+
+	fmt.Printf("%+v\n", domain)
+}
---
+++ registrar_test.go
@@ -0,0 +1,150 @@
+package cloudflare
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	registrarDomainPayload = `{
+		"id": "ea95132c15732412d22c1476fa83f27a",
+		"available": false,
+		"supported_tld": true,
+		"can_register": false,
+		"transfer_in": {
+			"unlock_domain": "ok",
+			"disable_privacy": "ok",
+			"enter_auth_code": "needed",
+			"approve_transfer": "unknown",
+			"accept_foa": "needed",
+			"can_cancel_transfer": true
+		},
+		"current_registrar": "Cloudflare",
+		"expires_at": "2019-08-28T23:59:59Z",
+		"registry_statuses": "ok,serverTransferProhibited",
+		"locked": false,
+		"created_at": "2018-08-28T17:26:26Z",
+		"updated_at": "2018-08-28T17:26:26Z",
+		"registrant_contact": {
+			"id": "ea95132c15732412d22c1476fa83f27a",
+			"first_name": "John",
+			"last_name": "Appleseed",
+			"organization": "Cloudflare, Inc.",
+			"address": "123 Sesame St.",
+			"address2": "Suite 430",
+			"city": "Austin",
+			"state": "TX",
+			"zip": "12345",
+			"country": "US",
+			"phone": "+1 123-123-1234",
+			"email": "user@example.com",
+			"fax": "123-867-5309"
+		}
+	}
+`
+)
+
+var (
+	createdAndModifiedTimestamp, _ = time.Parse(time.RFC3339, "2018-08-28T17:26:26Z")
+	expiresAtTimestamp, _          = time.Parse(time.RFC3339, "2019-08-28T23:59:59Z")
+	expectedRegistrarTransferIn    = RegistrarTransferIn{
+		UnlockDomain:      "ok",
+		DisablePrivacy:    "ok",
+		EnterAuthCode:     "needed",
+		ApproveTransfer:   "unknown",
+		AcceptFoa:         "needed",
+		CanCancelTransfer: true,
+	}
+	expectedRegistrarContact = RegistrantContact{
+		ID:           "ea95132c15732412d22c1476fa83f27a",
+		FirstName:    "John",
+		LastName:     "Appleseed",
+		Organization: "Cloudflare, Inc.",
+		Address:      "123 Sesame St.",
+		Address2:     "Suite 430",
+		City:         "Austin",
+		State:        "TX",
+		Zip:          "12345",
+		Country:      "US",
+		Phone:        "+1 123-123-1234",
+		Email:        "user@example.com",
+		Fax:          "123-867-5309",
+	}
+	expectedRegistrarDomain = RegistrarDomain{
+		ID:                "ea95132c15732412d22c1476fa83f27a",
+		Available:         false,
+		SupportedTLD:      true,
+		CanRegister:       false,
+		TransferIn:        expectedRegistrarTransferIn,
+		CurrentRegistrar:  "Cloudflare",
+		ExpiresAt:         expiresAtTimestamp,
+		RegistryStatuses:  "ok,serverTransferProhibited",
+		Locked:            false,
+		CreatedAt:         createdAndModifiedTimestamp,
+		UpdatedAt:         createdAndModifiedTimestamp,
+		RegistrantContact: expectedRegistrarContact,
+	}
+)
+
+func TestRegistrarDomain(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": {
+				"id": "ea95132c15732412d22c1476fa83f27a",
+				"available": false,
+				"supported_tld": true,
+				"can_register": false,
+				"transfer_in": {
+					"unlock_domain": "ok",
+					"disable_privacy": "ok",
+					"enter_auth_code": "needed",
+					"approve_transfer": "unknown",
+					"accept_foa": "needed",
+					"can_cancel_transfer": true
+				},
+				"current_registrar": "Cloudflare",
+				"expires_at": "2019-08-28T23:59:59Z",
+				"registry_statuses": "ok,serverTransferProhibited",
+				"locked": false,
+				"created_at": "2018-08-28T17:26:26Z",
+				"updated_at": "2018-08-28T17:26:26Z",
+				"registrant_contact": {
+					"id": "ea95132c15732412d22c1476fa83f27a",
+					"first_name": "John",
+					"last_name": "Appleseed",
+					"organization": "Cloudflare, Inc.",
+					"address": "123 Sesame St.",
+					"address2": "Suite 430",
+					"city": "Austin",
+					"state": "TX",
+					"zip": "12345",
+					"country": "US",
+					"phone": "+1 123-123-1234",
+					"email": "user@example.com",
+					"fax": "123-867-5309"
+				}
+			}
+		}
+		`)
+	}
+
+	mux.HandleFunc("/accounts/01a7362d577a6c3019a474fd6f485823/registrar/domains/cloudflare.com", handler)
+
+	actual, err := client.RegistrarDomain("01a7362d577a6c3019a474fd6f485823", "cloudflare.com")
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedRegistrarDomain, actual)
+	}
+}
