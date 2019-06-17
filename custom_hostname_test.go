f6fe9fbeb2bfc49c2df93adff561abb3e25bb93a
Dave Byrd <byrd.dave@gmail.com>
2019-06-16 19:50:01 -0500

    CUSTOM_ORIGIN_SERVER Adding custom_origin_server to CustomHostname struct

--- custom_hostname_test.go
+++ custom_hostname_test.go
@@ -82,7 +82,63 @@ func TestCustomHostname_CreateCustomHostname(t *testing.T) {
 		assert.Equal(t, want, response)
 	}
 }
+func TestCustomHostname_CreateCustomHostname_CustomOrigin(t *testing.T) {
+	setup()
+	defer teardown()
 
+	mux.HandleFunc("/zones/foo/custom_hostnames", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "POST", r.Method, "Expected method 'POST', got %s", r.Method)
+
+		w.Header().Set("content-type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprintf(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "id": "0d89c70d-ad9f-4843-b99f-6cc0252067e9",
+	"hostname": "app.example.com",
+	"custom_origin_server": "example.app.com",
+    "ssl": {
+      "status": "pending_validation",
+      "method": "cname",
+      "type": "dv",
+      "cname_target": "dcv.digicert.com",
+      "cname": "810b7d5f01154524b961ba0cd578acc2.app.example.com",
+      "settings": {
+        "http2": "on"
+      }
+    }
+  }
+}`)
+	})
+
+	response, err := client.CreateCustomHostname("foo", CustomHostname{Hostname: "app.example.com", CustomOriginServer: "example.app.com", SSL: CustomHostnameSSL{Method: "cname", Type: "dv"}})
+
+	want := &CustomHostnameResponse{
+		Result: CustomHostname{
+			ID:       "0d89c70d-ad9f-4843-b99f-6cc0252067e9",
+			Hostname: "app.example.com",
+			CustomOriginServer: "example.app.com",
+			SSL: CustomHostnameSSL{
+				Type:        "dv",
+				Method:      "cname",
+				Status:      "pending_validation",
+				CnameTarget: "dcv.digicert.com",
+				CnameName:   "810b7d5f01154524b961ba0cd578acc2.app.example.com",
+				Settings: CustomHostnameSSLSettings{
+					HTTP2: "on",
+				},
+			},
+		},
+		Response: Response{Success: true, Errors: []ResponseInfo{}, Messages: []ResponseInfo{}},
+	}
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, response)
+	}
+}
 func TestCustomHostname_CustomHostnames(t *testing.T) {
 	setup()
 	defer teardown()
