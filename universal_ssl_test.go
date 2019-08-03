f58e5855e713fecba3857283e6fb212b607bd931
Pierre Tondereau <pierre.tondereau@protonmail.com>
2019-08-03 22:15:43 +0200

    Add universal ssl verification details.

--- universal_ssl_test.go
+++ universal_ssl_test.go
@@ -78,3 +78,51 @@ func TestEditUniversalSSLSetting(t *testing.T) {
 		assert.Equal(t, want, got)
 	}
 }
+
+func TestUniversalSSLVerificationDetails(t *testing.T) {
+	setup()
+	defer teardown()
+
+	testZoneID := "abcd123"
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, r.Method, "GET", "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `{
+			"result": [{
+				"certificate_status": "active",
+				"verification_type": "cname",
+				"verification_status": true,
+				"verification_info": {
+					"record_name": "b3b90cfedd89a3e487d3e383c56c4267.example.com",
+					"record_target": "6979be7e4cfc9e5c603e31df7efac9cc60fee82d.comodoca.com"
+				},
+				"brand_check": false,
+				"validation_method": "txt",
+				"cert_pack_uuid": "a77f8bd7-3b47-46b4-a6f1-75cf98109948"
+			}]
+		}`)
+	}
+
+	mux.HandleFunc("/zones/"+testZoneID+"/ssl/verification", handler)
+
+	want := []UniversalSSLVerificationDetails{
+		{
+			CertificateStatus:  "active",
+			VerificationType:   "cname",
+			ValidationMethod:   "txt",
+			CertPackUUID:       "a77f8bd7-3b47-46b4-a6f1-75cf98109948",
+			VerificationStatus: true,
+			BrandCheck:         false,
+			VerificationInfo: UniversalSSLVerificationInfo{
+				RecordName:   "b3b90cfedd89a3e487d3e383c56c4267.example.com",
+				RecordTarget: "6979be7e4cfc9e5c603e31df7efac9cc60fee82d.comodoca.com",
+			},
+		},
+	}
+
+	got, err := client.UniversalSSLVerificationDetails(testZoneID)
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, got)
+	}
+}
