be0b36fe0bb414f48d54d50f4963be78e17e9b84
Jake Riesterer <jRiest@users.noreply.github.com>
2019-10-24 11:46:09 -0500

    Add support for worker bindings (#363)
    * Add support for worker bindings
    
    Currently this only handles "inherit" bindings, but it's already getting to be a fairly large change, so I wanted to break it up a bit before implementing other types of bindings.
    
    * Add support for Workers KV namespace bindings
    
    * Add support for WebAssembly module worker bindings
    
    * fixup! Add support for worker bindings

--- workers.go
+++ workers.go
@@ -1,8 +1,15 @@
 package cloudflare
 
 import (
+	"bytes"
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"time"
 
 	"github.com/pkg/errors"
@@ -12,6 +19,15 @@ import (
 type WorkerRequestParams struct {
 	ZoneID     string
 	ScriptName string
+}
+
+// WorkerScriptParams provides a worker script and the associated bindings
+type WorkerScriptParams struct {
+	Script string
+
+	// Bindings should be a map where the keys are the binding name, and the
+	// values are the binding content
+	Bindings map[string]WorkerBinding
 }
 
 // WorkerRoute aka filters are patterns used to enable or disable workers that match requests.
@@ -61,6 +77,104 @@ type WorkerListResponse struct {
 type WorkerScriptResponse struct {
 	Response
 	WorkerScript `json:"result"`
+}
+
+// Workers supports multiple types of bindings, e.g. KV namespaces or WebAssembly modules, and each type
+// of binding will be represented differently in the upload request body. At a high-level, every binding
+// will specify metadata, which is a JSON object with the properties "name" and "type". Some types of bindings
+// will also have additional metadata properties. For example, KV bindings also specify the KV namespace.
+// In addition to the metadata, some binding types may need to include additional data as part of the
+// multipart form. For example, WebAssembly bindings will include the contents of the WebAssembly module.
+
+// WorkerBinding is the generic interface implemented by all of
+// the various binding types
+type WorkerBinding interface {
+	// serialize is responsible for returning the binding metadata as well as an optionally
+	// returning a function that can modify the multipart form body. For example, this is used
+	// by WebAssembly bindings to add a new part containing the WebAssembly module contents.
+	serialize(bindingName string) (workerBindingMeta, workerBindingBodyWriter, error)
+}
+
+// workerBindingMeta is the metadata portion of the binding
+type workerBindingMeta = map[string]interface{}
+
+// workerBindingBodyWriter allows for a binding to add additional parts to the multipart body
+type workerBindingBodyWriter func(*multipart.Writer) error
+
+// WorkerInheritBinding will just persist whatever binding content was previously uploaded
+type WorkerInheritBinding struct {
+	// Optional parameter that allows for renaming a binding without changing
+	// its contents. If `OldName` is empty, the binding name will not be changed.
+	OldName string
+}
+
+func (b WorkerInheritBinding) serialize(bindingName string) (workerBindingMeta, workerBindingBodyWriter, error) {
+	meta := workerBindingMeta{
+		"name": bindingName,
+		"type": "inherit",
+	}
+
+	if b.OldName != "" {
+		meta["old_name"] = b.OldName
+	}
+
+	return meta, nil, nil
+}
+
+// WorkerKvNamespaceBinding is a binding to a Workers KV Namespace
+//
+// https://developers.cloudflare.com/workers/archive/api/resource-bindings/kv-namespaces/
+type WorkerKvNamespaceBinding struct {
+	NamespaceID string
+}
+
+func (b WorkerKvNamespaceBinding) serialize(bindingName string) (workerBindingMeta, workerBindingBodyWriter, error) {
+	if b.NamespaceID == "" {
+		return nil, nil, errors.Errorf(`NamespaceID for binding "%s" cannot be empty`, bindingName)
+	}
+
+	return workerBindingMeta{
+		"name":         bindingName,
+		"type":         "kv_namespace",
+		"namespace_id": b.NamespaceID,
+	}, nil, nil
+}
+
+// WorkerWebAssemblyBinding is a binding to a WebAssembly module
+//
+// https://developers.cloudflare.com/workers/archive/api/resource-bindings/webassembly-modules/
+type WorkerWebAssemblyBinding struct {
+	Module io.Reader
+}
+
+func (b WorkerWebAssemblyBinding) serialize(bindingName string) (workerBindingMeta, workerBindingBodyWriter, error) {
+	partName := getRandomPartName()
+
+	bodyWriter := func(mpw *multipart.Writer) error {
+		var hdr = textproto.MIMEHeader{}
+		hdr.Set("content-disposition", fmt.Sprintf(`form-data; name="%s"`, partName))
+		hdr.Set("content-type", "application/wasm")
+		pw, err := mpw.CreatePart(hdr)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(pw, b.Module)
+		return err
+	}
+
+	return workerBindingMeta{
+		"name": bindingName,
+		"type": "wasm_module",
+		"part": partName,
+	}, bodyWriter, nil
+}
+
+// Each binding that adds a part to the multipart form body will need
+// a unique part name so we just generate a random 128bit hex string
+func getRandomPartName() string {
+	randBytes := make([]byte, 16)
+	rand.Read(randBytes)
+	return hex.EncodeToString(randBytes)
 }
 
 // DeleteWorker deletes worker for a zone.
@@ -170,12 +284,30 @@ func (api *API) ListWorkerScripts() (WorkerListResponse, error) {
 // API reference: https://api.cloudflare.com/#worker-script-upload-worker
 func (api *API) UploadWorker(requestParams *WorkerRequestParams, data string) (WorkerScriptResponse, error) {
 	if requestParams.ScriptName != "" {
-		return api.uploadWorkerWithName(requestParams.ScriptName, data)
+		return api.uploadWorkerWithName(requestParams.ScriptName, "application/javascript", []byte(data))
 	}
-	uri := "/zones/" + requestParams.ZoneID + "/workers/script"
+	return api.uploadWorkerForZone(requestParams.ZoneID, "application/javascript", []byte(data))
+}
+
+// UploadWorkerWithBindings push raw script content and bindings for your worker
+//
+// API reference: https://api.cloudflare.com/#worker-script-upload-worker
+func (api *API) UploadWorkerWithBindings(requestParams *WorkerRequestParams, data *WorkerScriptParams) (WorkerScriptResponse, error) {
+	contentType, body, err := formatMultipartBody(data)
+	if err != nil {
+		return WorkerScriptResponse{}, err
+	}
+	if requestParams.ScriptName != "" {
+		return api.uploadWorkerWithName(requestParams.ScriptName, contentType, body)
+	}
+	return api.uploadWorkerForZone(requestParams.ZoneID, contentType, body)
+}
+
+func (api *API) uploadWorkerForZone(zoneID, contentType string, body []byte) (WorkerScriptResponse, error) {
+	uri := "/zones/" + zoneID + "/workers/script"
 	headers := make(http.Header)
-	headers.Set("Content-Type", "application/javascript")
-	res, err := api.makeRequestWithHeaders("PUT", uri, []byte(data), headers)
+	headers.Set("Content-Type", contentType)
+	res, err := api.makeRequestWithHeaders("PUT", uri, body, headers)
 	var r WorkerScriptResponse
 	if err != nil {
 		return r, errors.Wrap(err, errMakeRequestError)
@@ -187,19 +319,14 @@ func (api *API) UploadWorker(requestParams *WorkerRequestParams, data string) (W
 	return r, nil
 }
 
-// UploadWorkerWithName push raw script content for your worker.
-//
-// This is an enterprise only feature https://developers.cloudflare.com/workers/api/config-api-for-enterprise/
-//
-// API reference: https://api.cloudflare.com/#worker-script-upload-worker
-func (api *API) uploadWorkerWithName(scriptName string, data string) (WorkerScriptResponse, error) {
+func (api *API) uploadWorkerWithName(scriptName, contentType string, body []byte) (WorkerScriptResponse, error) {
 	if api.AccountID == "" {
 		return WorkerScriptResponse{}, errors.New("account ID required for enterprise only request")
 	}
 	uri := "/accounts/" + api.AccountID + "/workers/scripts/" + scriptName
 	headers := make(http.Header)
-	headers.Set("Content-Type", "application/javascript")
-	res, err := api.makeRequestWithHeaders("PUT", uri, []byte(data), headers)
+	headers.Set("Content-Type", contentType)
+	res, err := api.makeRequestWithHeaders("PUT", uri, body, headers)
 	var r WorkerScriptResponse
 	if err != nil {
 		return r, errors.Wrap(err, errMakeRequestError)
@@ -209,6 +336,77 @@ func (api *API) uploadWorkerWithName(scriptName string, data string) (WorkerScri
 		return r, errors.Wrap(err, errUnmarshalError)
 	}
 	return r, nil
+}
+
+// Returns content-type, body, error
+func formatMultipartBody(params *WorkerScriptParams) (string, []byte, error) {
+	var buf = &bytes.Buffer{}
+	var mpw = multipart.NewWriter(buf)
+	defer mpw.Close()
+
+	// Write metadata part
+	scriptPartName := "script"
+	meta := struct {
+		BodyPart string              `json:"body_part"`
+		Bindings []workerBindingMeta `json:"bindings"`
+	}{
+		BodyPart: scriptPartName,
+		Bindings: make([]workerBindingMeta, 0, len(params.Bindings)),
+	}
+
+	bodyWriters := make([]workerBindingBodyWriter, 0, len(params.Bindings))
+	for name, b := range params.Bindings {
+		bindingMeta, bodyWriter, err := b.serialize(name)
+		if err != nil {
+			return "", nil, err
+		}
+
+		meta.Bindings = append(meta.Bindings, bindingMeta)
+		bodyWriters = append(bodyWriters, bodyWriter)
+	}
+
+	var hdr = textproto.MIMEHeader{}
+	hdr.Set("content-disposition", fmt.Sprintf(`form-data; name="%s"`, "metadata"))
+	hdr.Set("content-type", "application/json")
+	pw, err := mpw.CreatePart(hdr)
+	if err != nil {
+		return "", nil, err
+	}
+	metaJSON, err := json.Marshal(meta)
+	if err != nil {
+		return "", nil, err
+	}
+	_, err = pw.Write(metaJSON)
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Write script part
+	hdr = textproto.MIMEHeader{}
+	hdr.Set("content-disposition", fmt.Sprintf(`form-data; name="%s"`, scriptPartName))
+	hdr.Set("content-type", "application/javascript")
+	pw, err = mpw.CreatePart(hdr)
+	if err != nil {
+		return "", nil, err
+	}
+	_, err = pw.Write([]byte(params.Script))
+	if err != nil {
+		return "", nil, err
+	}
+
+	// Write other bindings with parts
+	for _, w := range bodyWriters {
+		if w != nil {
+			err = w(mpw)
+			if err != nil {
+				return "", nil, err
+			}
+		}
+	}
+
+	mpw.Close()
+
+	return mpw.FormDataContentType(), buf.Bytes(), nil
 }
 
 // CreateWorkerRoute creates worker route for a zone
