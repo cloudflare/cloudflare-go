88295673144e8fb78a3a9076c09097775b2c0aeb
Tim Bart <tim@fewagainstmany.com>
2020-08-13 03:53:59 -0700

    feat(workers): Adds support for Workers secrets (#475)
    SetWorkersSecret creates or updates a secret
    DeleteWorkersSecret deletes a secret
    ListWorkersSecrets lists all secrets

---
+++ workers_secrets_test.go
@@ -0,0 +1,111 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWorkers_SetWorkersSecret(t *testing.T) {
+	setup(UsingAccount("foo"))
+	defer teardown()
+
+	response := `{
+		"result": {
+			"name" : "my-secret",
+			"type": "secret_text"
+		},
+		"success": true,
+		"errors": [],
+		"messages": []
+	}`
+
+	mux.HandleFunc("/accounts/foo/workers/scripts/test-script/secrets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "PUT", r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/javascript")
+		fmt.Fprintf(w, response)
+	})
+	req := &WorkersPutSecretRequest{
+		Name: "my-secret",
+		Text: "super-secret",
+	}
+	res, err := client.SetWorkersSecret(context.Background(), "test-script", req)
+	want := WorkersPutSecretResponse{
+		successResponse,
+		WorkersSecret{
+			Name: "test",
+			Type: "secret_text",
+		},
+	}
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want.Response, res.Response)
+	}
+}
+
+func TestWorkers_DeleteWorkersSecret(t *testing.T) {
+	setup(UsingAccount("foo"))
+	defer teardown()
+
+	response := `{
+		"result": {
+			"name" : "test",
+			"type": "secret_text"
+		},
+		"success": true,
+		"errors": [],
+		"messages": []
+	}`
+
+	mux.HandleFunc("/accounts/foo/workers/scripts/test-script/secrets/my-secret", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "DELETE", r.Method, "Expected method 'DELETE', got %s", r.Method)
+		w.Header().Set("content-type", "application/javascript")
+		fmt.Fprintf(w, response)
+	})
+
+	res, err := client.DeleteWorkersSecret(context.Background(), "test-script", "my-secret")
+	want := successResponse
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, res)
+	}
+}
+
+func TestWorkers_ListWorkersSecret(t *testing.T) {
+	setup(UsingAccount("foo"))
+	defer teardown()
+
+	response := `{
+		"result": [{
+			"name" : "my-secret",
+			"type": "secret_text"
+		}],
+		"success": true,
+		"errors": [],
+		"messages": []
+	}`
+
+	mux.HandleFunc("/accounts/foo/workers/scripts/test-script/secrets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "GET", r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/javascript")
+		fmt.Fprintf(w, response)
+	})
+
+	res, err := client.ListWorkersSecrets(context.Background(), "test-script")
+	want := WorkersListSecretsResponse{
+		successResponse,
+		[]WorkersSecret{
+			{
+				Name: "my-secret",
+				Type: "secret_text",
+			},
+		},
+	}
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want.Response, res.Response)
+	}
+}
