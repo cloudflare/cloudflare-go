808b65fca3557e3a2380e9226cd5f8cc27b4bf40
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-02-24 12:20:45 +1100

    Better error handling for API errors
    This introduces a change to how we raise and interact with exceptions
    raised by the API responses.
    
    For quite some time, interacting with the API response errors has been
    difficult due to them spitting out parsed JSON (as a string). This means
    if you need to check the type of exception in order to recover or retry,
    you need to use an unreliable `strings.Contains` check. This also
    extends to internally when we want to show the user an error, we had a
    `errorFromResponse` function which would return the HTTP status code and
    whatever was in the `errors` array.
    
    To improve the situation I'm proposing a revamp of our error handling by
    exposing various methods on `APIRequestError` struct as helpers to get
    the information clients may need to better handle errors. Here are some
    examples:
    
    ### Return the whole error message
    
    ```go
    fmt.Println(err.Error())
    
    // => "HTTP status 400: Authentication error (10000)"
    ```
    
    ### Return only the HTTP status
    
    ```go
    fmt.Println(err.(*cloudflare.APIRequestError).HTTPStatusCode())
    
    // => 400
    ```
    
    ### Check if the response was rate limited
    
    ```go
    fmt.Println(err.(*cloudflare.APIRequestError).HTTPStatusCode())
    
    // => false
    ```
    
    In order to achieve this, we'll need to update almost every method that
    makes an API call to reflect the following (example from
    `createAccessRule` in firewall.go):
    
    ```diff
    @@ -218,7 +218,7 @@ func (api *API) createAccessRule(prefix string, accessRule AccessRule) (*AccessR
            uri := prefix + "/firewall/access_rules/rules"
            res, err := api.makeRequest("POST", uri, accessRule)
            if err != nil {
    -               return nil, errors.Wrap(err, errMakeRequestError)
    +               return nil, err.(*APIRequestError)
            }
    
            response := &AccessRuleResponse{}
    ```
    
    Personally, I think this is a better situation for the maintainers and
    consumers of this library as we'll be better equipped to handle
    differing error scenarios. The Terraform provider will also benefit from
    these changes for things like retries.

--- errors.go
+++ errors.go
@@ -1,52 +1,78 @@
 package cloudflare
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // Error messages
 const (
 	errEmptyCredentials          = "invalid credentials: key & email must not be empty"
 	errEmptyAPIToken             = "invalid credentials: API Token must not be empty"
 	errMakeRequestError          = "error from makeRequest"
 	errUnmarshalError            = "error unmarshalling the JSON response"
+	errUnmarshalErrorBody        = "error unmarshalling the JSON response error body"
 	errRequestNotSuccessful      = "error reported by API"
 	errMissingAccountID          = "account ID is empty and must be provided"
 	errOperationStillRunning     = "bulk operation did not finish before timeout"
 	errOperationUnexpectedStatus = "bulk operation returned an unexpected status"
 )
 
-var _ Error = &UserError{}
-
-// Error represents an error returned from this library.
-type Error interface {
-	error
-	// Raised when user credentials or configuration is invalid.
-	User() bool
-	// Raised when a parsing error (e.g. JSON) occurs.
-	Parse() bool
-	// Raised when a network error occurs.
-	Network() bool
-	// Contains the most recent error.
+// APIRequestError is a type of error raised by API calls made by this library.
+type APIRequestError struct {
+	StatusCode int
+	Message    string
+	ErrorCode  int
 }
 
-// UserError represents a user-generated error.
-type UserError struct {
-	Err error
+func (e APIRequestError) Error() string {
+	errString := ""
+	errString += fmt.Sprintf("HTTP status %d", e.StatusCode)
+
+	if e.Message != "" {
+		errString += fmt.Sprintf(": %s", e.Message)
+	}
+
+	if e.ErrorCode != 0 {
+		errString += fmt.Sprintf(" (%d)", e.ErrorCode)
+	}
+
+	return errString
 }
 
-// User is a user-caused error.
-func (e *UserError) User() bool {
-	return true
+// HTTPStatusCode exposes the HTTP status from the error response encountered.
+func (e APIRequestError) HTTPStatusCode() int {
+	return e.StatusCode
 }
 
-// Network error.
-func (e *UserError) Network() bool {
-	return false
+// ErrorMessage exposes the first error message from the error response
+// encountered.
+func (e *APIRequestError) ErrorMessage() string {
+	return e.Message
 }
 
-// Parse error.
-func (e *UserError) Parse() bool {
-	return true
+// InternalErrorCode exposes the first internal error code from the error
+// response encountered.
+func (e *APIRequestError) InternalErrorCode() int {
+	return e.ErrorCode
 }
 
-// Error wraps the underlying error.
-func (e *UserError) Error() string {
-	return e.Err.Error()
+// ServiceError returns a boolean whether or not the raised error was caused by
+// an internal service.
+func (e *APIRequestError) ServiceError() bool {
+	return e.StatusCode >= http.StatusInternalServerError
+}
+
+// ClientError returns a boolean whether or not the raised error was caused by
+// something client side.
+func (e *APIRequestError) ClientError() bool {
+	return e.StatusCode >= http.StatusBadRequest &&
+		e.StatusCode <= http.StatusInternalServerError &&
+		e.StatusCode != http.StatusTooManyRequests
+}
+
+// ClientRateLimited returns a boolean whether or not the raised error was
+// caused by too many requests from the client.
+func (e *APIRequestError) ClientRateLimited() bool {
+	return e.StatusCode == http.StatusTooManyRequests
 }
