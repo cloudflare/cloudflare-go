b4def24e9d8d8807c249b21a81fa99a446cc0c16
favonia <favonia@gmail.com>
2021-08-01 04:22:00 -0500

    style: move mocking functions out

--- zone_test.go
+++ zone_test.go
@@ -30,6 +30,124 @@ func mustParseTime(s string) time.Time {
 	return t
 }
 
+func mockZone(i int) *Zone {
+	return &Zone{
+		ID:      mockID(fmt.Sprintf("zone %d", i)),
+		Name:    fmt.Sprintf("%d.example.org", i),
+		DevMode: 0,
+		OriginalNS: []string{
+			"linda.ns.cloudflare.com",
+			"merlin.ns.cloudflare.com",
+		},
+		OriginalRegistrar: "cloudflare, inc. (id: 1910)",
+		OriginalDNSHost:   "",
+		CreatedOn:         mustParseTime("2021-07-28T05:06:20.736244Z"),
+		ModifiedOn:        mustParseTime("2021-07-28T05:06:20.736244Z"),
+		NameServers: []string{
+			"abby.ns.cloudflare.com",
+			"noel.ns.cloudflare.com",
+		},
+		Owner: Owner{
+			ID:        mockID("Test Account"),
+			Email:     "",
+			Name:      "Test Account",
+			OwnerType: "organization",
+		},
+		Permissions: []string{
+			"#access:read",
+			"#analytics:read",
+			"#auditlogs:read",
+			"#billing:read",
+			"#dns_records:read",
+			"#lb:read",
+			"#legal:read",
+			"#logs:read",
+			"#member:read",
+			"#organization:read",
+			"#ssl:read",
+			"#stream:read",
+			"#subscription:read",
+			"#waf:read",
+			"#webhooks:read",
+			"#worker:read",
+			"#zone:read",
+			"#zone_settings:read",
+		},
+		Plan: ZonePlan{
+			ZonePlanCommon: ZonePlanCommon{
+				ID:       "0feeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+				Name:     "Free Website",
+				Currency: "USD",
+			},
+			IsSubscribed:      false,
+			CanSubscribe:      false,
+			LegacyID:          "free",
+			LegacyDiscount:    false,
+			ExternallyManaged: false,
+		},
+		PlanPending: ZonePlan{
+			ZonePlanCommon: ZonePlanCommon{
+				ID: "",
+			},
+			IsSubscribed:      false,
+			CanSubscribe:      false,
+			LegacyID:          "",
+			LegacyDiscount:    false,
+			ExternallyManaged: false,
+		},
+		Status: "active",
+		Paused: false,
+		Type:   "full",
+		Host: struct {
+			Name    string
+			Website string
+		}{
+			Name:    "",
+			Website: "",
+		},
+		VanityNS:    nil,
+		Betas:       nil,
+		DeactReason: "",
+		Meta: ZoneMeta{
+			PageRuleQuota:     3,
+			WildcardProxiable: false,
+			PhishingDetected:  false,
+		},
+		Account: Account{
+			ID:   mockID("Test Account"),
+			Name: "Test Account",
+		},
+		VerificationKey: "",
+	}
+}
+
+func mockResultInfo(total, page, start, count int) *ResultInfo {
+	return &ResultInfo{
+		Page:       page,
+		PerPage:    50,
+		TotalPages: (total + 49) / 50,
+		Count:      count,
+		Total:      total,
+	}
+}
+
+func mockZonesResponse(total, page, start, count int) *ZonesResponse {
+	zones := make([]Zone, count)
+	for i := range zones {
+		zones[i] = *mockZone(start + i)
+	}
+
+	return &ZonesResponse{
+		Result:     zones,
+		ResultInfo: *mockResultInfo(total, page, start, count),
+		Response: Response{
+			Success:  true,
+			Errors:   []ResponseInfo{},
+			Messages: []ResponseInfo{},
+		},
+	}
+}
+
 func TestZoneAnalyticsDashboard(t *testing.T) {
 	setup()
 	defer teardown()
@@ -1225,124 +1343,6 @@ func TestListZones(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mockZone := func(i int) Zone {
-		return Zone{
-			ID:      mockID(fmt.Sprintf("zone %d", i)),
-			Name:    fmt.Sprintf("%d.example.org", i),
-			DevMode: 0,
-			OriginalNS: []string{
-				"linda.ns.cloudflare.com",
-				"merlin.ns.cloudflare.com",
-			},
-			OriginalRegistrar: "cloudflare, inc. (id: 1910)",
-			OriginalDNSHost:   "",
-			CreatedOn:         mustParseTime("2021-07-28T05:06:20.736244Z"),
-			ModifiedOn:        mustParseTime("2021-07-28T05:06:20.736244Z"),
-			NameServers: []string{
-				"abby.ns.cloudflare.com",
-				"noel.ns.cloudflare.com",
-			},
-			Owner: Owner{
-				ID:        mockID("Test Account"),
-				Email:     "",
-				Name:      "Test Account",
-				OwnerType: "organization",
-			},
-			Permissions: []string{
-				"#access:read",
-				"#analytics:read",
-				"#auditlogs:read",
-				"#billing:read",
-				"#dns_records:read",
-				"#lb:read",
-				"#legal:read",
-				"#logs:read",
-				"#member:read",
-				"#organization:read",
-				"#ssl:read",
-				"#stream:read",
-				"#subscription:read",
-				"#waf:read",
-				"#webhooks:read",
-				"#worker:read",
-				"#zone:read",
-				"#zone_settings:read",
-			},
-			Plan: ZonePlan{
-				ZonePlanCommon: ZonePlanCommon{
-					ID:       "0feeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-					Name:     "Free Website",
-					Currency: "USD",
-				},
-				IsSubscribed:      false,
-				CanSubscribe:      false,
-				LegacyID:          "free",
-				LegacyDiscount:    false,
-				ExternallyManaged: false,
-			},
-			PlanPending: ZonePlan{
-				ZonePlanCommon: ZonePlanCommon{
-					ID: "",
-				},
-				IsSubscribed:      false,
-				CanSubscribe:      false,
-				LegacyID:          "",
-				LegacyDiscount:    false,
-				ExternallyManaged: false,
-			},
-			Status: "active",
-			Paused: false,
-			Type:   "full",
-			Host: struct {
-				Name    string
-				Website string
-			}{
-				Name:    "",
-				Website: "",
-			},
-			VanityNS:    nil,
-			Betas:       nil,
-			DeactReason: "",
-			Meta: ZoneMeta{
-				PageRuleQuota:     3,
-				WildcardProxiable: false,
-				PhishingDetected:  false,
-			},
-			Account: Account{
-				ID:   mockID("Test Account"),
-				Name: "Test Account",
-			},
-			VerificationKey: "",
-		}
-	}
-
-	mockResultInfo := func(total, page, start, count int) ResultInfo {
-		return ResultInfo{
-			Page:       page,
-			PerPage:    50,
-			TotalPages: (total + 49) / 50,
-			Count:      count,
-			Total:      total,
-		}
-	}
-
-	mockZonesResponse := func(total, page, start, count int) ZonesResponse {
-		zones := make([]Zone, count)
-		for i := range zones {
-			zones[i] = mockZone(start + i)
-		}
-
-		return ZonesResponse{
-			Result:     zones,
-			ResultInfo: mockResultInfo(total, page, start, count),
-			Response: Response{
-				Success:  true,
-				Errors:   []ResponseInfo{},
-				Messages: []ResponseInfo{},
-			},
-		}
-	}
-
 	const (
 		total     = 392
 		totalPage = (total + 49) / 50
@@ -1399,6 +1399,6 @@ func TestListZones(t *testing.T) {
 	}
 
 	for i, zone := range zones {
-		assert.Equal(t, mockZone(i), zone)
+		assert.Equal(t, *mockZone(i), zone)
 	}
 }
