cce63748502ae45a9a04b825586d675a6d2281dc
Venceslas <onevan.mail@gmail.com>
2021-10-13 12:08:29 +0100

    removed space

--- rulesets.go
+++ rulesets.go
@@ -203,7 +203,7 @@ type RulesetRuleActionParametersHTTPHeader struct {
 
 type RulesetRuleActionParametersOverrides struct {
 	Enabled    bool                                    `json:"enabled,omitempty"`
-	Action     string								   `json:"action,omitempty"`
+	Action     string                                  `json:"action,omitempty"`
 	Categories []RulesetRuleActionParametersCategories `json:"categories,omitempty"`
 	Rules      []RulesetRuleActionParametersRules      `json:"rules,omitempty"`
 }
