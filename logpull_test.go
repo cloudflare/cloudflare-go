69441918563b228883e51d0b586664cc951ed207
Cyril Tovena <cyril.tovena@gmail.com>
2021-11-18 11:35:40 +0100

    Add support LogpullReceived and LogpullFields.
    This PR also include support for gzip content encoding while reading response.
    
    Signed-off-by: Cyril Tovena <cyril.tovena@gmail.com>

--- logpull_test.go
+++ logpull_test.go
@@ -1,10 +1,13 @@
 package cloudflare
 
 import (
+	"compress/gzip"
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -59,4 +62,86 @@ func TestSetLogpullRetentionFlag(t *testing.T) {
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
 	}
+}
+
+func TestLogpullFields(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprintf(w, `{
+		"foo": "bar",
+		"field_1": "value_1",
+		"field_2": "value_2"
+	}`)
+	}
+
+	mux.HandleFunc("/zones/e49c6317afd4ceabe24c6e11d2dad231/logs/received/fields", handler)
+
+	actual, err := client.LogpullFields(context.Background(), "e49c6317afd4ceabe24c6e11d2dad231")
+	if assert.NoError(t, err) {
+		assert.Equal(t, map[string]string{
+			"foo":     "bar",
+			"field_1": "value_1",
+			"field_2": "value_2",
+		}, actual)
+	}
+}
+
+func TestLogpullReceived(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var (
+		want = []string{
+			`{"field1":"value1", "field2":"value2"}`,
+			`{"field11":"value11", "field22":"value22"}`,
+		}
+		fields       = []string{"field1", "field2"}
+		count  int64 = 10
+		sample       = 0.1
+		start        = time.Unix(60, 0)
+		end          = time.Unix(120, 0)
+	)
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		assert.Equal(t, r.URL.Query().Get("count"), "10")
+		assert.Equal(t, r.URL.Query().Get("sample"), "0.1")
+		assert.Equal(t, r.URL.Query().Get("fields"), strings.Join(fields, ","))
+		assert.Equal(t, r.URL.Query().Get("start"), fmt.Sprintf("%d", start.UnixNano()))
+		assert.Equal(t, r.URL.Query().Get("end"), fmt.Sprintf("%d", end.UnixNano()))
+
+		w.Header().Set("Content-Encoding", "gzip")
+		wg := gzip.NewWriter(w)
+		for _, line := range want {
+			fmt.Fprintln(wg, line)
+		}
+		assert.NoError(t, wg.Close())
+	}
+
+	mux.HandleFunc("/zones/e49c6317afd4ceabe24c6e11d2dad231/logs/received", handler)
+
+	l, err := client.LogpullReceived(context.Background(), "e49c6317afd4ceabe24c6e11d2dad231", start, end, LogpullReceivedOption{
+		Fields: fields,
+		Count:  &count,
+		Sample: &sample,
+	})
+	if !assert.NoError(t, err) {
+		return
+	}
+	defer l.Close()
+	var actual []string
+	for l.Next() {
+		actual = append(actual, string(l.Line()))
+		assert.NoError(t, l.Err())
+		f, err := l.Fields()
+		if assert.NoError(t, err) {
+			assert.Len(t, f, 2)
+		}
+	}
+
+	assert.Equal(t, want, actual)
 }
