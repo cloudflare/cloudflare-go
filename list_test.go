eb7df792fa3fa59871ec0b102c34cfccab4822e4
Diogo Sousa <dsousa@cloudflare.com>
2022-05-31 15:35:44 +0100

    Added support for redirect lists.

--- list_test.go
+++ list_test.go
@@ -301,14 +301,14 @@ func TestListsItemsIP(t *testing.T) {
 	want := []ListItem{
 		{
 			ID:         "2c0fc9fa937b11eaa1b71c4d701ab86e",
-			IP:         "192.0.2.1",
+			IP:         StringPtr("192.0.2.1"),
 			Comment:    "Private IP address",
 			CreatedOn:  &createdOn,
 			ModifiedOn: &modifiedOn,
 		},
 		{
 			ID:         "2c0fc9fa937b11eaa1b71c4d701ab86e",
-			IP:         "192.0.2.2",
+			IP:         StringPtr("192.0.2.2"),
 			Comment:    "Another Private IP address",
 			CreatedOn:  &createdOn,
 			ModifiedOn: &modifiedOn,
@@ -318,6 +318,111 @@ func TestListsItemsIP(t *testing.T) {
 	actual, err := client.ListListItems(context.Background(), ListListItemsParams{
 		AccountID: testAccountID, ID: "2c0fc9fa937b11eaa1b71c4d701ab86e",
 	})
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestListsItemsRedirect(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+
+		if len(r.URL.Query().Get("cursor")) > 0 && r.URL.Query().Get("cursor") == "yyy" {
+			fmt.Fprint(w, `{
+			"result": [
+				{
+      				"id": "1c0fc9fa937b11eaa1b71c4d701ab86e",
+      				"redirect": {
+						"source_url": "www.3fonteinen.be",
+						"target_url": "https://shop.3fonteinen.be"
+					},
+      				"comment": "3F redirect",
+      				"created_on": "2020-01-01T08:00:00Z",
+      				"modified_on": "2020-01-10T14:00:00Z"
+    			}
+  			],
+  			"result_info": {
+    			"cursors": {
+					"before": "xxx"
+				}
+			},
+			"success": true,
+			"errors": [],
+			"messages": []
+		}`)
+		} else {
+			fmt.Fprint(w, `{
+			"result": [
+				{
+      				"id": "0c0fc9fa937b11eaa1b71c4d701ab86e",
+      				"redirect": {
+						"source_url": "http://cloudflare.com",
+						"include_subdomains": true,
+						"target_url": "https://cloudflare.com",
+						"status_code": 302,
+						"preserve_query_string": true,
+						"subpath_matching": true,
+						"preserve_path_suffix": false
+					},
+      				"comment": "Cloudflare http redirect",
+      				"created_on": "2020-01-01T08:00:00Z",
+      				"modified_on": "2020-01-10T14:00:00Z"
+    			}
+  			],
+  			"result_info": {
+    			"cursors": {
+					"before": "xxx",
+					"after": "yyy"
+				}
+			},
+			"success": true,
+			"errors": [],
+			"messages": []
+		}`)
+		}
+	}
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/rules/lists/0c0fc9fa937b11eaa1b71c4d701ab86e/items", handler)
+
+	createdOn, _ := time.Parse(time.RFC3339, "2020-01-01T08:00:00Z")
+	modifiedOn, _ := time.Parse(time.RFC3339, "2020-01-10T14:00:00Z")
+
+	want := []ListItem{
+		{
+			ID: "0c0fc9fa937b11eaa1b71c4d701ab86e",
+			Redirect: &Redirect{
+				SourceUrl:           "http://cloudflare.com",
+				IncludeSubdomains:   BoolPtr(true),
+				TargetUrl:           "https://cloudflare.com",
+				StatusCode:          IntPtr(302),
+				PreserveQueryString: BoolPtr(true),
+				SubpathMatching:     BoolPtr(true),
+				PreservePathSuffix:  BoolPtr(false),
+			},
+			Comment:    "Cloudflare http redirect",
+			CreatedOn:  &createdOn,
+			ModifiedOn: &modifiedOn,
+		},
+		{
+			ID: "1c0fc9fa937b11eaa1b71c4d701ab86e",
+			Redirect: &Redirect{
+				SourceUrl: "www.3fonteinen.be",
+				TargetUrl: "https://shop.3fonteinen.be",
+			},
+			Comment:    "3F redirect",
+			CreatedOn:  &createdOn,
+			ModifiedOn: &modifiedOn,
+		},
+	}
+
+	actual, err := client.ListListItems(
+		context.Background(),
+		ListListItemsParams{AccountID: testAccountID, ID: "0c0fc9fa937b11eaa1b71c4d701ab86e"},
+	)
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
 	}
@@ -352,11 +457,57 @@ func TestCreateListItemsIP(t *testing.T) {
 		AccountID: testAccountID,
 		ID:        "2c0fc9fa937b11eaa1b71c4d701ab86e",
 		Items: []ListItemCreateRequest{{
-			IP:      "192.0.2.1",
+			IP:      StringPtr("192.0.2.1"),
 			Comment: "Private IP",
 		}, {
-			IP:      "192.0.2.2",
+			IP:      StringPtr("192.0.2.2"),
 			Comment: "Another Private IP",
+		}}})
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestCreateListItemsRedirect(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method, "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+			"result": {
+				"operation_id": "4da8780eeb215e6cb7f48dd981c4ea02"
+			},
+			"success": true,
+			"errors": [],
+			"messages": []
+		}`)
+	}
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/rules/lists/0c0fc9fa937b11eaa1b71c4d701ab86e/items", handler)
+
+	want := ListItemCreateResponse{}
+	want.Success = true
+	want.Errors = []ResponseInfo{}
+	want.Messages = []ResponseInfo{}
+	want.Result.OperationID = "4da8780eeb215e6cb7f48dd981c4ea02"
+
+	actual, err := client.CreateListItemsAsync(context.Background(), ListCreateItemsParams{
+		AccountID: testAccountID,
+		ID:        "0c0fc9fa937b11eaa1b71c4d701ab86e",
+		Items: []ListItemCreateRequest{{
+			Redirect: &Redirect{
+				SourceUrl: "www.3fonteinen.be",
+				TargetUrl: "https://shop.3fonteinen.be",
+			},
+			Comment: "redirect 3F",
+		}, {
+			Redirect: &Redirect{
+				SourceUrl: "www.cf.com",
+				TargetUrl: "https://cloudflare.com",
+			},
+			Comment: "Redirect cf",
 		}}})
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
@@ -392,11 +543,57 @@ func TestReplaceListItemsIP(t *testing.T) {
 		AccountID: testAccountID,
 		ID:        "2c0fc9fa937b11eaa1b71c4d701ab86e",
 		Items: []ListItemCreateRequest{{
-			IP:      "192.0.2.1",
+			IP:      StringPtr("192.0.2.1"),
 			Comment: "Private IP",
 		}, {
-			IP:      "192.0.2.2",
+			IP:      StringPtr("192.0.2.2"),
 			Comment: "Another Private IP",
+		}}})
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestReplaceListItemsRedirect(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+			"result": {
+				"operation_id": "4da8780eeb215e6cb7f48dd981c4ea02"
+			},
+			"success": true,
+			"errors": [],
+			"messages": []
+		}`)
+	}
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/rules/lists/2c0fc9fa937b11eaa1b71c4d701ab86e/items", handler)
+
+	want := ListItemCreateResponse{}
+	want.Success = true
+	want.Errors = []ResponseInfo{}
+	want.Messages = []ResponseInfo{}
+	want.Result.OperationID = "4da8780eeb215e6cb7f48dd981c4ea02"
+
+	actual, err := client.ReplaceListItemsAsync(context.Background(), ListReplaceItemsParams{
+		AccountID: testAccountID,
+		ID:        "2c0fc9fa937b11eaa1b71c4d701ab86e",
+		Items: []ListItemCreateRequest{{
+			Redirect: &Redirect{
+				SourceUrl: "www.3fonteinen.be",
+				TargetUrl: "https://shop.3fonteinen.be",
+			},
+			Comment: "redirect 3F",
+		}, {
+			Redirect: &Redirect{
+				SourceUrl: "www.cf.com",
+				TargetUrl: "https://cloudflare.com",
+			},
+			Comment: "Redirect cf",
 		}}})
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
@@ -468,7 +665,7 @@ func TestGetListItemIP(t *testing.T) {
 
 	want := ListItem{
 		ID:         "2c0fc9fa937b11eaa1b71c4d701ab86e",
-		IP:         "192.0.2.1",
+		IP:         StringPtr("192.0.2.1"),
 		Comment:    "Private IP address",
 		CreatedOn:  &createdOn,
 		ModifiedOn: &modifiedOn,
