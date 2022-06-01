3ab118ec6ad1c82bf61855cf62e0b2a9b40745b4
Amos Paul <amos@cloudflare.com>
2022-06-01 12:53:03 -0700

    Updating API usage of IPsec tunnels

--- magic_transit_ipsec_tunnel.go
+++ magic_transit_ipsec_tunnel.go
@@ -16,16 +16,31 @@ const (
 	errMagicTransitIPsecTunnelNotDeleted  = "When trying to delete IPsec tunnel, API returned deleted: false"
 )
 
+type RemoteIdentities struct {
+	HexID  string `json:"hex_id"`
+	FQDNID string `json:"fqdn_id"`
+	UserID string `json:"user_id"`
+}
+
+// MagicTransitIPsecTunnelPskMetadata contains metadata associated with PSK.
+type MagicTransitIPsecTunnelPskMetadata struct {
+	LastGeneratedOn *time.Time `json:"last_generated_on,omitempty"`
+}
+
 // MagicTransitIPsecTunnel contains information about an IPsec tunnel.
 type MagicTransitIPsecTunnel struct {
-	ID                 string     `json:"id,omitempty"`
-	CreatedOn          *time.Time `json:"created_on,omitempty"`
-	ModifiedOn         *time.Time `json:"modified_on,omitempty"`
-	Name               string     `json:"name"`
-	CustomerEndpoint   string     `json:"customer_endpoint"`
-	CloudflareEndpoint string     `json:"cloudflare_endpoint"`
-	InterfaceAddress   string     `json:"interface_address"`
-	Description        string     `json:"description,omitempty"`
+	ID                 string                              `json:"id,omitempty"`
+	CreatedOn          *time.Time                          `json:"created_on,omitempty"`
+	ModifiedOn         *time.Time                          `json:"modified_on,omitempty"`
+	Name               string                              `json:"name"`
+	CustomerEndpoint   string                              `json:"customer_endpoint"`
+	CloudflareEndpoint string                              `json:"cloudflare_endpoint"`
+	InterfaceAddress   string                              `json:"interface_address"`
+	Description        string                              `json:"description,omitempty"`
+	HealthCheck        *MagicTransitTunnelHealthcheck      `json:"health_check,omitempty"`
+	Psk                string                              `json:"psk,omitempty"`
+	PskMetadata        *MagicTransitIPsecTunnelPskMetadata `json:"psk_metadata,omitempty"`
+	RemoteIdentities   *RemoteIdentities                   `json:"remote_identities,omitempty"`
 }
 
 // ListMagicTransitIPsecTunnelsResponse contains a response including IPsec tunnels.
@@ -64,6 +79,15 @@ type DeleteMagicTransitIPsecTunnelResponse struct {
 	Result struct {
 		Deleted            bool                    `json:"deleted"`
 		DeletedIPsecTunnel MagicTransitIPsecTunnel `json:"deleted_ipsec_tunnel"`
+	} `json:"result"`
+}
+
+// GenerateMagicTransitIPsecTunnelPSKResponse contains a response after generating IPsec Tunnel.
+type GenerateMagicTransitIPsecTunnelPSKResponse struct {
+	Response
+	Result struct {
+		Psk         string                              `json:"psk"`
+		PskMetadata *MagicTransitIPsecTunnelPskMetadata `json:"psk_metadata"`
 	} `json:"result"`
 }
 
@@ -168,4 +192,23 @@ func (api *API) DeleteMagicTransitIPsecTunnel(ctx context.Context, accountID str
 	}
 
 	return result.Result.DeletedIPsecTunnel, nil
+}
+
+// GenerateMagicTransitIPsecTunnelPSK generates a pre shared key (psk) for an IPsec tunnel
+//
+// API reference: https://api.cloudflare.com/#magic-ipsec-tunnels-generate-pre-shared-key-psk-for-ipsec-tunnels
+func (api *API) GenerateMagicTransitIPsecTunnelPSK(ctx context.Context, accountID string, id string) (string, *MagicTransitIPsecTunnelPskMetadata, error) {
+	uri := fmt.Sprintf("/accounts/%s/magic/ipsec_tunnels/%s/psk_generate", accountID, id)
+	res, err := api.makeRequestContext(ctx, http.MethodPost, uri, nil)
+
+	if err != nil {
+		return "", nil, err
+	}
+
+	result := GenerateMagicTransitIPsecTunnelPSKResponse{}
+	if err := json.Unmarshal(res, &result); err != nil {
+		return "", nil, errors.Wrap(err, errUnmarshalError)
+	}
+
+	return result.Result.Psk, result.Result.PskMetadata, nil
 }
