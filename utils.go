d427065382f6f454bfd85563c8e3f2cc1c64fa34
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-07-13 10:01:25 +1000

    tests: provide a `loadFixture` helper to clean up JSON outputs

--- utils.go
+++ utils.go
@@ -1,7 +1,10 @@
 package cloudflare
 
 import (
+	"fmt"
 	"net/url"
+	"os"
+	"path/filepath"
 
 	"github.com/google/go-querystring/query"
 )
@@ -10,4 +13,16 @@ import (
 func buildURI(path string, options interface{}) string {
 	v, _ := query.Values(options)
 	return (&url.URL{Path: path, RawQuery: v.Encode()}).String()
+}
+
+// loadFixture takes a series of path components and returns the JSON fixture at
+// that locationassociated.
+func loadFixture(parts ...string) string {
+	paths := []string{"testdata", "fixtures"}
+	paths = append(paths, parts...)
+	b, err := os.ReadFile(filepath.Join(paths...) + ".json")
+	if err != nil {
+		fmt.Print(err)
+	}
+	return string(b)
 }
