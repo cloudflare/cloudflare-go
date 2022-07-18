c25a907202da14c71634ff238ec8155e545cdb34
Denis Davydov <ddavydov@cloudflare.com>
2022-07-18 09:11:35 +0100

    Add dynamic redirect phase support for rulesets

--- rulesets_test.go
+++ rulesets_test.go
@@ -354,6 +354,95 @@ func TestGetRuleset_SetCacheSettings(t *testing.T) {
 	}
 }
 
+func TestGetRuleset_RedirectFromValue(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+      "result": {
+        "id": "70339d97bdb34195bbf054b1ebe81f76",
+        "name": "Cloudflare Redirect Rules Ruleset",
+        "description": "This ruleset provides redirect from value",
+        "kind": "zone",
+        "version": "1",
+        "rules": [
+          {
+            "id": "78723a9e0c7c4c6dbec5684cb766231d",
+            "version": "1",
+            "action": "redirect",
+            "action_parameters": {
+				"from_value": {
+					"status_code": 301,
+					"target_url": {
+						"value": "some_host.com"
+					},
+					"preserve_query_string": true
+				}
+			},
+			"description": "Set dynamic redirect from value",
+			"last_updated": "2020-12-18T09:28:09.655749Z",
+			"ref": "272936dc447b41fe976255ff6b768ec0",
+			"enabled": true
+          }
+        ],
+        "last_updated": "2020-12-18T09:28:09.655749Z",
+        "phase": "http_request_dynamic_redirect"
+      },
+      "success": true,
+      "errors": [],
+      "messages": []
+    }`)
+	}
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/rulesets/b232b534beea4e00a21dcbb7a8a545e9", handler)
+	mux.HandleFunc("/zones/"+testZoneID+"/rulesets/b232b534beea4e00a21dcbb7a8a545e9", handler)
+
+	lastUpdated, _ := time.Parse(time.RFC3339, "2020-12-18T09:28:09.655749Z")
+
+	rules := []RulesetRule{{
+		ID:      "78723a9e0c7c4c6dbec5684cb766231d",
+		Version: "1",
+		Action:  string(RulesetRuleActionRedirect),
+		ActionParameters: &RulesetRuleActionParameters{
+			FromValue: &RulesetRuleActionParametersFromValue{
+				StatusCode: 301,
+				TargetURL: RulesetRuleActionParametersTargetURL{
+					Value: "some_host.com",
+				},
+				PreserveQueryString: true,
+			},
+		},
+		Description: "Set dynamic redirect from value",
+		LastUpdated: &lastUpdated,
+		Ref:         "272936dc447b41fe976255ff6b768ec0",
+		Enabled:     true,
+	}}
+
+	want := Ruleset{
+		ID:          "70339d97bdb34195bbf054b1ebe81f76",
+		Name:        "Cloudflare Redirect Rules Ruleset",
+		Description: "This ruleset provides redirect from value",
+		Kind:        string(RulesetKindZone),
+		Version:     "1",
+		LastUpdated: &lastUpdated,
+		Phase:       string(RulesetPhaseHTTPRequestDynamicRedirect),
+		Rules:       rules,
+	}
+
+	zoneActual, err := client.GetZoneRuleset(context.Background(), testZoneID, "b232b534beea4e00a21dcbb7a8a545e9")
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, zoneActual)
+	}
+
+	accountActual, err := client.GetAccountRuleset(context.Background(), testAccountID, "b232b534beea4e00a21dcbb7a8a545e9")
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, accountActual)
+	}
+}
+
 func TestCreateRuleset(t *testing.T) {
 	setup()
 	defer teardown()
