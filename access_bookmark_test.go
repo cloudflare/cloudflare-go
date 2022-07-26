cd82560f67dfbd7f2ad8b441efdac4588260aef2
Justin Holmes <justin.ryan.holmes@icloud.com>
2022-07-26 15:28:30 -0500

    Use pointers for boolean fields in Access Applications

--- access_bookmark_test.go
+++ access_bookmark_test.go
@@ -50,7 +50,7 @@ func TestAccessBookmarks(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 		CreatedAt:          &createdAt,
 		UpdatedAt:          &updatedAt,
 	}}
@@ -104,7 +104,7 @@ func TestAccessBookmark(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 		CreatedAt:          &createdAt,
 		UpdatedAt:          &updatedAt,
 	}
@@ -157,7 +157,7 @@ func TestCreateAccessBookmarks(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 		CreatedAt:          &createdAt,
 		UpdatedAt:          &updatedAt,
 	}
@@ -168,7 +168,7 @@ func TestCreateAccessBookmarks(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 	})
 
 	if assert.NoError(t, err) {
@@ -181,7 +181,7 @@ func TestCreateAccessBookmarks(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 	})
 
 	if assert.NoError(t, err) {
@@ -220,7 +220,7 @@ func TestUpdateAccessBookmark(t *testing.T) {
 		Name:               "Example Site",
 		Domain:             "example.com",
 		LogoURL:            "https://www.example.com/example.png",
-		AppLauncherVisible: true,
+		AppLauncherVisible: BoolPtr(true),
 		CreatedAt:          &createdAt,
 		UpdatedAt:          &updatedAt,
 	}
