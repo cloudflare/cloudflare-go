8917e44aeef840919ff2a1168c6a005cc0b2b738
Robert Kopaczewski <rk@23doors.com>
2022-08-30 04:18:58 +0200

    pr changes

--- cloudflare.go
+++ cloudflare.go
@@ -473,33 +473,7 @@ type RawResponse struct {
 
 // Raw makes a HTTP request with user provided params and returns the
 // result as untouched JSON.
-func (api *API) Raw(method, endpoint string, data interface{}) (json.RawMessage, error) {
-	res, err := api.makeRequest(method, endpoint, data)
-	if err != nil {
-		return nil, err
-	}
-
-	var r RawResponse
-	if err := json.Unmarshal(res, &r); err != nil {
-		return nil, fmt.Errorf("%s: %w", errUnmarshalError, err)
-	}
-	return r.Result, nil
-}
-
-func (api *API) RawContext(ctx context.Context, method, endpoint string, data interface{}) (json.RawMessage, error) {
-	res, err := api.makeRequestContext(ctx, method, endpoint, data)
-	if err != nil {
-		return nil, err
-	}
-
-	var r RawResponse
-	if err := json.Unmarshal(res, &r); err != nil {
-		return nil, fmt.Errorf("%s: %w", errUnmarshalError, err)
-	}
-	return r.Result, nil
-}
-
-func (api *API) RawContextWithHeaders(ctx context.Context, method, endpoint string, data interface{}, headers http.Header) (json.RawMessage, error) {
+func (api *API) Raw(ctx context.Context, method, endpoint string, data interface{}, headers http.Header) (json.RawMessage, error) {
 	res, err := api.makeRequestContextWithHeaders(ctx, method, endpoint, data, headers)
 	if err != nil {
 		return nil, err
