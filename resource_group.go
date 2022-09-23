61f081fd35288bb195bfaea54b8080f2b2dbdc47
Ian Mobbs <imobbs@cloudflare.com>
2022-09-23 12:56:46 -0700

    ACCT-4459: Add resource group utility methods

--- resource_group.go
+++ resource_group.go
@@ -1,5 +1,7 @@
 package cloudflare
 
+import "fmt"
+
 type ResourceGroup struct {
 	ID    string            `json:"id"`
 	Name  string            `json:"name"`
@@ -14,4 +16,32 @@ type Scope struct {
 
 type ScopeObject struct {
 	Key string `json:"key"`
+}
+
+func NewResourceGroupForZone(zone Zone) ResourceGroup {
+	return NewResourceGroup(fmt.Sprintf("com.cloudflare.api.account.zone.%s", zone.ID))
+}
+
+func NewResourceGroupForAccount(account Account) ResourceGroup {
+	return NewResourceGroup(fmt.Sprintf("com.cloudflare.api.account.%s", account.ID))
+}
+
+func NewResourceGroup(key string) ResourceGroup {
+	scope := Scope{
+		Key: key,
+		ScopeObjects: []ScopeObject{
+			ScopeObject{
+				Key: "*",
+			},
+		},
+	}
+	resourceGroup := ResourceGroup{
+		ID:   "",
+		Name: key,
+		Meta: map[string]string{
+			"editable": "false",
+		},
+		Scope: scope,
+	}
+	return resourceGroup
 }
