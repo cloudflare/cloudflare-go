53c41b8ce26a2a884099b0a8c4b1f4d5f76802e1
Ian Mobbs <imobbs@cloudflare.com>
2022-09-29 14:53:28 -0700

    ACCT-4459: Add resource group documentation and testing

--- resource_group.go
+++ resource_group.go
@@ -18,14 +18,20 @@ type ScopeObject struct {
 	Key string `json:"key"`
 }
 
+// NewResourceGroupForZone takes an existing zone and provides a resource group
+// to be used within a Policy that allows access to that zone
 func NewResourceGroupForZone(zone Zone) ResourceGroup {
 	return NewResourceGroup(fmt.Sprintf("com.cloudflare.api.account.zone.%s", zone.ID))
 }
 
+// NewResourceGroupForAccount takes an existing zone and provides a resource group
+// to be used within a Policy that allows access to that account
 func NewResourceGroupForAccount(account Account) ResourceGroup {
 	return NewResourceGroup(fmt.Sprintf("com.cloudflare.api.account.%s", account.ID))
 }
 
+// NewResourceGroup takes a Cloudflare-formatted key (e.g. 'com.cloudflare.api.%s') and
+// returns a resource group to be used within a Policy to allow access to that resource
 func NewResourceGroup(key string) ResourceGroup {
 	scope := Scope{
 		Key: key,
