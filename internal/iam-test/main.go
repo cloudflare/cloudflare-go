3c87263ac8ed50114ae1d8c9e86e15dda5b46f96
jtdo <jtdo@cloudflare.com>
2022-09-29 12:32:18 -0700

    ACCT-4593: Refactor to reduce clutter, list out more endpoints to test

--- internal/iam-test/main.go
+++ internal/iam-test/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/cloudflare/cloudflare-go"
 	"log"
 	"os"
@@ -15,6 +14,26 @@ func main() {
 	// Construct a new API object using a global API key or scoped API token (preferred)
 	var options []cloudflare.Option
 	options = append(options, cloudflare.BaseURL("https://api.staging.cloudflare.com/client/v4"))
+
+	// Non ZoLA
+	nonZolaApi, err := cloudflare.New(
+		os.Getenv("CLOUDFLARE_NONZOLA_API_KEY"),
+		os.Getenv("CLOUDFLARE_NONZOLA_API_EMAIL"),
+		options...,
+	)
+	if err != nil {
+		log.Fatalf("ERROR: Failed to construct new API object: %v\n", err)
+	}
+	ctx := context.Background()
+
+	log.Printf("Testing API with non-ZoLA account\n")
+	runApiTest(
+		ctx,
+		nonZolaApi,
+		os.Getenv("CLOUDFLARE_NONZOLA_ACCOUNT_NAME"),
+	)
+
+	// ZoLA
 	zolaApi, err := cloudflare.New(
 		os.Getenv("CLOUDFLARE_ZOLA_API_KEY"),
 		os.Getenv("CLOUDFLARE_ZOLA_API_EMAIL"),
@@ -24,85 +43,103 @@ func main() {
 	if err != nil {
 		log.Fatalf("ERROR: Failed to construct new API object: %v\n", err)
 	}
-	ctx := context.Background()
+	ctx = context.Background()
 
-	// Testing variables
-	accountName := os.Getenv("CLOUDFLARE_ZOLA_ACCOUNT_NAME")
-	newMemberEmail := os.Getenv("CLOUDFLARE_ZOLA_INVITEE")
+	log.Printf("Testing API with ZoLA account\n")
+	runApiTest(
+		ctx,
+		zolaApi,
+		os.Getenv("CLOUDFLARE_ZOLA_ACCOUNT_NAME"),
+	)
+}
 
-	/*
-		TODO: currently using a staging zola account, need nonzola
+func runApiTest(ctx context.Context, api *cloudflare.API, accountName string) {
+	// User's account memberships endpoints
+	// TODO:
+	// 	 list user memberships
+	//	 get user membership details
+	//	 update user memberships
+	//	 delete user memberships
 
-		TODO: List of endpoints rom API docs:
-		- [ ] user account memberships
-			- [ ] list memberships
-			- [ ] get membership details
-			- [ ] update memberships
-			- [ ] delete memberships
-		- [ ] accounts
-			- [ ] list accounts
-			- [ ] get account details
-			- [ ] update account
-		- [ ] account members
-			- [ ] list
-			- [ ] add
-			- [ ] update
-			- [ ] remove
-		- [ ] account roles
-			- [ ] list
-			- [ ] get details
-	*/
-
-	// User account memberships endpoints
-	fmt.Printf("\n\n---\n" +
-		"User account memberships" +
-		"\n---\n\n")
-
-	user, err := zolaApi.UserDetails(ctx)
+	// - Get user details
+	user, err := api.UserDetails(ctx)
 	if err != nil {
 		log.Printf("ERROR: Failed to fetch user details: %v\n", err)
 	}
-	debugPrint("user", user)
-
-	updatedUser, err := zolaApi.UpdateUser(ctx, &user)
-	if err != nil {
-		log.Printf("ERROR: Failed to fetch user details: %v\n", err)
-	}
-	debugPrint("updatedUser", updatedUser)
+	debugPrint("[user] get user details", user)
 
 	// Accounts endpoints
-	fmt.Printf("\n\n---\n" +
-		"Accounts" +
-		"\n---\n\n")
+	// - List accounts
+	accounts, _, err := api.Accounts(ctx, cloudflare.AccountsListParams{})
+	if err != nil {
+		log.Printf("ERROR: Failed to fetch list of accounts: %v\n", err)
+	}
+	for i := 0; i < len(accounts); i++ {
+		debugPrint("", accounts[i].Name)
+	}
 
-	accounts, resultInfo, err := zolaApi.Accounts(ctx, cloudflare.AccountsListParams{Name: accountName})
+	// - Get account details
+	accountId := accounts[0].ID
+	account, _, err := api.Account(ctx, accountId)
 	if err != nil {
 		log.Printf("ERROR: Failed to fetch account: %v\n", err)
 	}
-	accountId := accounts[0].ID
+	debugPrint("[account] get account details", account)
 
-	debugPrint("account", accounts)
-	debugPrint("resultInfo", resultInfo)
-	debugPrint("accountId", accountId)
+	// - TODO: Update account
+	//updatedAccount, err := api.UpdateAccount(ctx, accountId, account)
+	//if err != nil {
+	//	log.Printf("ERROR: Failed to fetch account: %v\n", err)
+	//}
+	//debugPrint("[account] update account", updatedAccount)
 
 	// Account memberships endpoints
-	fmt.Printf("\n\n---\n" +
-		"Account memberships" +
-		"\n---\n\n")
-
-	newAccountMember, err := zolaApi.CreateAccountMember(ctx, accountId, newMemberEmail, []string{})
-	if err != nil {
-		log.Printf("ERROR: Failed to create account member: %v\n", err)
-	}
-	debugPrint("newAccountMember", newAccountMember)
-
-	accountMembers, resultInfo, err := zolaApi.AccountMembers(ctx, accountId, cloudflare.PaginationOptions{})
+	// - List all members
+	accountMembers, _, err := api.AccountMembers(ctx, accountId, cloudflare.PaginationOptions{})
 	if err != nil {
 		log.Printf("ERROR: Failed to fetch account members: %v\n", err)
 	}
 	for i := 0; i < len(accountMembers); i++ {
 		debugPrint("", accountMembers[i].User.Email)
 	}
+
+	// - TODO: Add account member
+	//newMemberEmail := "jtdocf+cfgotest@gmail.com" // TODO: use testing email
+	//var roles []string                            // TODO: need valid roles
+	//newAccountMember, err := api.CreateAccountMember(ctx, accountId, newMemberEmail, roles)
+	//if err != nil {
+	//	log.Printf("ERROR: Failed to create account member: %v\n", err)
+	//}
+	//debugPrint("[account member] newAccountMember", newAccountMember)
+
+	// - TODO: Update account member
+	//updatedAccountMember, err := api.UpdateAccountMember(ctx, accountId, "someUserId", newAccountMember)
+	//if err != nil {
+	//	log.Printf("ERROR: Failed to update account member: %v\n", err)
+	//}
+	//debugPrint("[account member] updatedAccountMember", updatedAccountMember)
+
+	// - TODO: Delete account member (and validate)
+	//err = api.DeleteAccountMember(ctx, accountId, "someUserId")
+	//if err != nil {
+	//	log.Printf("ERROR: Failed to delete account member: %v\n", err)
+	//}
+
+	// Account roles
+	// List all
+	accountRoles, err := api.AccountRoles(ctx, accountId)
+	if err != nil {
+		log.Printf("ERROR: Failed to fetch list of account roles: %v\n", err)
+	}
+	debugPrint("[account roles] list roles", accountRoles)
+
+	// Get details
+	roleId := accountRoles[0].ID
+	accountRole, err := api.AccountRole(ctx, accountId, roleId)
+	if err != nil {
+		log.Printf("ERROR: Failed to fetch list of account roles: %v\n", err)
+	}
+	debugPrint("[account roles] get role details", accountRole)
 
 }
 
