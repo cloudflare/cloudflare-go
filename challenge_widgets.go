0ed26113c22270838210166a800ef06a00853597
Jacob White <jake@jwhite.network>
2022-11-11 22:13:54 -0500

    Add support for challenge widget API

---
+++ challenge_widgets.go
@@ -0,0 +1,210 @@
+package cloudflare
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var ErrMissingSiteKey = errors.New("required site key missing")
+
+type ChallengeWidget struct {
+	SiteKey    string     `json:"sitekey"`
+	Secret     string     `json:"secret,omitempty"`
+	CreatedOn  *time.Time `json:"created_on,omitempty"`
+	ModifiedOn *time.Time `json:"modified_on,omitempty"`
+	Name       string     `json:"name"`
+	Domains    []string   `json:"domains"`
+	Type       string     `json:"type"`
+}
+
+type ChallengeWidgetResponse struct {
+	Response
+	Result ChallengeWidget `json:"result"`
+}
+
+type ListChallengeWidgetRequest struct {
+	ResultInfo
+	Direction string `url:"direction,omitempty"`
+	Order     string `url:"order,omitempty"`
+}
+
+type ListChallengeWidgetResponse struct {
+	Response
+	ResultInfo `json:"result_info"`
+	Result     []ChallengeWidget `json:"result"`
+}
+
+type RotateChallengeWidgetRequest struct {
+	SiteKey               string
+	InvalidateImmediately bool `json:"invalidate_immediately,omitempty"`
+}
+
+// CreateChallengeWidget creates a new challenge widgets.
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-properties
+func (api *API) CreateChallengeWidget(ctx context.Context, rc *ResourceContainer, params ChallengeWidget) (ChallengeWidget, error) {
+	if rc.Identifier == "" {
+		return ChallengeWidget{}, ErrMissingAccountID
+	}
+	uri := fmt.Sprintf("/accounts/%s/challenges/widgets", rc.Identifier)
+	res, err := api.makeRequestContext(ctx, "POST", uri, params)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+	var r ChallengeWidgetResponse
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+	return r.Result, nil
+}
+
+// ListChallengeWidget lists challenge widgets.
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-list-challenge-widgets
+func (api *API) ListChallengeWidget(ctx context.Context, rc *ResourceContainer, params ListChallengeWidgetRequest) ([]ChallengeWidget, *ResultInfo, error) {
+	if rc.Identifier == "" {
+		return []ChallengeWidget{}, &ResultInfo{}, ErrMissingAccountID
+	}
+	autoPaginate := true
+	if params.PerPage >= 1 || params.Page >= 1 {
+		autoPaginate = false
+	}
+
+	if params.PerPage < 1 {
+		params.PerPage = 50
+	}
+
+	if params.Page < 1 {
+		params.Page = 1
+	}
+
+	var widgets []ChallengeWidget
+	var r ListChallengeWidgetResponse
+	for {
+		uri := buildURI(fmt.Sprintf("/accounts/%s/challenges/widgets", rc.Identifier), params)
+
+		res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+		if err != nil {
+			return []ChallengeWidget{}, &ResultInfo{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+		}
+		err = json.Unmarshal(res, &r)
+		if err != nil {
+			return []ChallengeWidget{}, &ResultInfo{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+		}
+		widgets = append(widgets, r.Result...)
+		params.ResultInfo = r.ResultInfo.Next()
+		if params.ResultInfo.Done() || !autoPaginate {
+			break
+		}
+	}
+
+	return widgets, &r.ResultInfo, nil
+}
+
+// GetChallengeWidget shows a single challenge widget configuration..
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-challenge-widget-details
+func (api *API) GetChallengeWidget(ctx context.Context, rc *ResourceContainer, SiteKey string) (ChallengeWidget, error) {
+	if rc.Identifier == "" {
+		return ChallengeWidget{}, ErrMissingAccountID
+	}
+
+	if SiteKey == "" {
+		return ChallengeWidget{}, ErrMissingSiteKey
+	}
+
+	uri := fmt.Sprintf("/accounts/%s/challenges/widgets/%s", rc.Identifier, SiteKey)
+	res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+	var r ChallengeWidgetResponse
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return r.Result, nil
+}
+
+// UpdateChallengeWidget update the configuration of a widget.
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-update-a-challenge-widget
+func (api *API) UpdateChallengeWidget(ctx context.Context, rc *ResourceContainer, widget ChallengeWidget) (ChallengeWidget, error) {
+	if rc.Identifier == "" {
+		return ChallengeWidget{}, ErrMissingAccountID
+	}
+	if widget.SiteKey == "" {
+		return ChallengeWidget{}, ErrMissingSiteKey
+	}
+	uri := fmt.Sprintf("/accounts/%s/challenges/widgets/%s", rc.Identifier, widget.SiteKey)
+	res, err := api.makeRequestContext(ctx, http.MethodPut, uri, widget)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+	var r ChallengeWidgetResponse
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+	return r.Result, nil
+}
+
+// RotateChallengeWidget generates a new secret key for this widget. If invalidate_immediately is set to false, the previous secret remains valid for 2 hours.
+//
+// Note that secrets cannot be rotated again during the grace period.
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-rotate-secret-for-a-challenge-widget
+func (api *API) RotateChallengeWidget(ctx context.Context, rc *ResourceContainer, param RotateChallengeWidgetRequest) (ChallengeWidget, error) {
+	if rc.Identifier == "" {
+		return ChallengeWidget{}, ErrMissingAccountID
+	}
+	if param.SiteKey == "" {
+		return ChallengeWidget{}, ErrMissingSiteKey
+	}
+
+	uri := fmt.Sprintf("/accounts/%s/challenges/widgets/%s/rotate_secret", rc.Identifier, param.SiteKey)
+	res, err := api.makeRequestContext(ctx, http.MethodPost, uri, param)
+
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+	var r ChallengeWidgetResponse
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return ChallengeWidget{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+	return r.Result, nil
+}
+
+// DeleteChallengeWidget delete a challenge widget.
+//
+// API reference: https://api.cloudflare.com/#challenge-widgets-delete-a-challenge-widget
+func (api *API) DeleteChallengeWidget(ctx context.Context, rc *ResourceContainer, SiteKey string) error {
+	if rc.Identifier == "" {
+		return ErrMissingAccountID
+	}
+
+	if SiteKey == "" {
+		return ErrMissingSiteKey
+	}
+	uri := fmt.Sprintf("/accounts/%s/challenges/widgets/%s", rc.Identifier, SiteKey)
+
+	res, err := api.makeRequestContext(ctx, http.MethodDelete, uri, nil)
+	if err != nil {
+		return fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+
+	var r ChallengeWidgetResponse
+	err = json.Unmarshal(res, &r)
+	if err != nil {
+		return fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+	return nil
+}
---
+++ challenge_widgets_test.go
@@ -0,0 +1,307 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const testChallengeWidgetSiteKey = "0x4AAF00AAAABn0R22HWm-YUc"
+
+var (
+	challengeWidgetCreatedOn, _  = time.Parse(time.RFC3339, "2014-01-01T05:20:00.123123Z")
+	challengeWidgetModifiedOn, _ = time.Parse(time.RFC3339, "2014-01-01T05:20:00.123123Z")
+	expectedChallengeWidget      = ChallengeWidget{
+		SiteKey:    "0x4AAF00AAAABn0R22HWm-YUc",
+		Secret:     "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+		CreatedOn:  &challengeWidgetCreatedOn,
+		ModifiedOn: &challengeWidgetModifiedOn,
+		Name:       "blog.cloudflare.com login form",
+		Domains: []string{
+			"203.0.113.1",
+			"cloudflare.com",
+			"blog.example.com",
+		},
+		Type: "invisible",
+	}
+)
+
+func TestChallengeWidgets_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method, "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+    "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+    "created_on": "2014-01-01T05:20:00.123123Z",
+    "modified_on": "2014-01-01T05:20:00.123123Z",
+    "name": "blog.cloudflare.com login form",
+    "domains": [
+      "203.0.113.1",
+      "cloudflare.com",
+      "blog.example.com"
+    ],
+    "type": "invisible"
+  }
+}`)
+	})
+
+	// Make sure missing account ID is thrown
+	_, err := client.CreateChallengeWidget(context.Background(), AccountIdentifier(""), ChallengeWidget{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	out, err := client.CreateChallengeWidget(context.Background(), AccountIdentifier(testAccountID), ChallengeWidget{
+		Name: "blog.cloudflare.com login form",
+		Type: "invisible",
+		Domains: []string{
+			"203.0.113.1",
+			"cloudflare.com",
+			"blog.example.com",
+		},
+	})
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChallengeWidget, out, "create challenge_widgets structs not equal")
+	}
+}
+
+func TestChallengeWidgets_List(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": [
+    {
+      "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+      "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+      "created_on": "2014-01-01T05:20:00.123123Z",
+      "modified_on": "2014-01-01T05:20:00.123123Z",
+      "name": "blog.cloudflare.com login form",
+      "domains": [
+        "203.0.113.1",
+        "cloudflare.com",
+        "blog.example.com"
+      ],
+      "type": "invisible"
+    }
+  ],
+  "result_info": {
+    "page": 1,
+    "per_page": 20,
+    "count": 1,
+    "total_count": 2000
+  }
+}`)
+	})
+
+	_, _, err := client.ListChallengeWidget(context.Background(), AccountIdentifier(""), ListChallengeWidgetRequest{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	out, results, err := client.ListChallengeWidget(context.Background(), AccountIdentifier(testAccountID), ListChallengeWidgetRequest{})
+	if assert.NoError(t, err) {
+		assert.Equal(t, 1, len(out), "expected 1 challenge_widgets")
+		assert.Equal(t, 20, results.PerPage, "expected 20 per page")
+		assert.Equal(t, expectedChallengeWidget, out[0], "list challenge_widgets structs not equal")
+	}
+}
+
+func TestChallengeWidgets_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets/"+testChallengeWidgetSiteKey, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+    "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+    "created_on": "2014-01-01T05:20:00.123123Z",
+    "modified_on": "2014-01-01T05:20:00.123123Z",
+    "name": "blog.cloudflare.com login form",
+    "domains": [
+      "203.0.113.1",
+      "cloudflare.com",
+      "blog.example.com"
+    ],
+    "type": "invisible"
+  }
+}`)
+	})
+
+	_, err := client.GetChallengeWidget(context.Background(), AccountIdentifier(""), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	_, err = client.GetChallengeWidget(context.Background(), AccountIdentifier(testAccountID), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingSiteKey, err)
+	}
+
+	out, err := client.GetChallengeWidget(context.Background(), AccountIdentifier(testAccountID), testChallengeWidgetSiteKey)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChallengeWidget, out, "get challenge_widgets structs not equal")
+	}
+}
+
+func TestChallengeWidgets_Update(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets/"+testChallengeWidgetSiteKey, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+    "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+    "created_on": "2014-01-01T05:20:00.123123Z",
+    "modified_on": "2014-01-01T05:20:00.123123Z",
+    "name": "blog.cloudflare.com login form",
+    "domains": [
+      "203.0.113.1",
+      "cloudflare.com",
+      "blog.example.com"
+    ],
+    "type": "invisible"
+  }
+}`)
+	})
+
+	_, err := client.UpdateChallengeWidget(context.Background(), AccountIdentifier(""), ChallengeWidget{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	_, err = client.UpdateChallengeWidget(context.Background(), AccountIdentifier(testAccountID), ChallengeWidget{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingSiteKey, err)
+	}
+
+	out, err := client.UpdateChallengeWidget(context.Background(), AccountIdentifier(testAccountID), ChallengeWidget{
+		SiteKey: testChallengeWidgetSiteKey,
+	})
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChallengeWidget, out, "update challenge_widgets structs not equal")
+	}
+}
+
+func TestChallengeWidgets_RotateSecret(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets/"+testChallengeWidgetSiteKey+"/rotate_secret", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method, "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+    "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+    "created_on": "2014-01-01T05:20:00.123123Z",
+    "modified_on": "2014-01-01T05:20:00.123123Z",
+    "name": "blog.cloudflare.com login form",
+    "domains": [
+      "203.0.113.1",
+      "cloudflare.com",
+      "blog.example.com"
+    ],
+    "type": "invisible"
+  }
+}`)
+	})
+
+	// Make sure missing account ID is thrown
+	_, err := client.RotateChallengeWidget(context.Background(), AccountIdentifier(""), RotateChallengeWidgetRequest{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	_, err = client.RotateChallengeWidget(context.Background(), AccountIdentifier(testAccountID), RotateChallengeWidgetRequest{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingSiteKey, err)
+	}
+
+	out, err := client.RotateChallengeWidget(context.Background(), AccountIdentifier(testAccountID), RotateChallengeWidgetRequest{SiteKey: testChallengeWidgetSiteKey})
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedChallengeWidget, out, "rotate challenge_widgets structs not equal")
+	}
+}
+
+func TestChallengeWidgets_Delete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/challenges/widgets/"+testChallengeWidgetSiteKey, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodDelete, r.Method, "Expected method 'DELETE', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+    "sitekey": "0x4AAF00AAAABn0R22HWm-YUc",
+    "secret": "0x4AAF00AAAABn0R22HWm098HVBjhdsYUc",
+    "created_on": "2014-01-01T05:20:00.123123Z",
+    "modified_on": "2014-01-01T05:20:00.123123Z",
+    "name": "blog.cloudflare.com login form",
+    "domains": [
+      "203.0.113.1",
+      "cloudflare.com",
+      "blog.example.com"
+    ],
+    "type": "invisible"
+  }
+}`)
+	})
+
+	// Make sure missing account ID is thrown
+	err := client.DeleteChallengeWidget(context.Background(), AccountIdentifier(""), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	err = client.DeleteChallengeWidget(context.Background(), AccountIdentifier(testAccountID), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingSiteKey, err)
+	}
+
+	err = client.DeleteChallengeWidget(context.Background(), AccountIdentifier(testAccountID), testChallengeWidgetSiteKey)
+	assert.NoError(t, err)
+}
