a10a008547af82207f9d084c09b73ff3e6e9f0b9
Troy Jones <troyj@indeed.com>
2023-08-30 16:22:06 -0600

    Add DCV Delegation UUID

---
+++ dcv_delegation.go
@@ -0,0 +1,39 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/goccy/go-json"
+)
+
+type DCVDelegation struct {
+	UUID string `json:"uuid"`
+}
+
+// DCVDelegationResponse represents the response from the dcv_delegation/uuid endpoint.
+type DCVDelegationResponse struct {
+	Result DCVDelegation `json:"result"`
+	Response
+	ResultInfo `json:"result_info"`
+}
+
+// GetDCVDelegation gets a zone DCV Delegation UUID
+//
+// TODO: Add API documentation
+func (api *API) GetDCVDelegation(ctx context.Context, rc *ResourceContainer) (DCVDelegation, ResultInfo, error) {
+	uri := fmt.Sprintf("/zones/%s/dcv_delegation/uuid", rc.Identifier)
+
+	res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return DCVDelegation{}, ResultInfo{}, err
+	}
+	var dcvResponse DCVDelegationResponse
+	err = json.Unmarshal(res, &dcvResponse)
+	if err != nil {
+		return DCVDelegation{}, ResultInfo{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return dcvResponse.Result, dcvResponse.ResultInfo, nil
+}
