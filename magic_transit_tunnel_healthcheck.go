14b3cea10a73fea0b4e7a4f52c5ac5dbf6b6c0b0
Christian Ehrig <cehrig@cloudflare.com>
2024-02-08 20:11:24 +0100

    Adds IPsec tunnel healthcheck direction & rate parameters

--- magic_transit_tunnel_healthcheck.go
+++ magic_transit_tunnel_healthcheck.go
@@ -2,7 +2,9 @@ package cloudflare
 
 // MagicTransitTunnelHealthcheck contains information about a tunnel health check.
 type MagicTransitTunnelHealthcheck struct {
-	Enabled bool   `json:"enabled"`
-	Target  string `json:"target,omitempty"`
-	Type    string `json:"type,omitempty"`
+	Enabled   bool   `json:"enabled"`
+	Target    string `json:"target,omitempty"`
+	Type      string `json:"type,omitempty"`
+	Rate      string `json:"rate,omitempty"`
+	Direction string `json:"direction,omitempty"`
 }
