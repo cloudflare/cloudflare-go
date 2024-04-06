b3c241246da1788a25f024a1dbafc869919e6f4c
Arkadiusz Kraus <11319923+arkadiuss@users.noreply.github.com>
2024-04-07 00:14:18 +0200

    add tests for listing and replacing hostnames, add creating client certs

--- api_shield_client_certificates.go
+++ api_shield_client_certificates.go
@@ -24,11 +24,34 @@ type ListHostnameAssociationParams struct {
 	CertificateID string `url:"mtls_certificate_id,omitempty"`
 }
 
-type UpdateHostnameAssociationParams struct {
-	HostnameAssociation
+type ReplaceHostnameAssociationParams struct {
+	CertificateID string   `json:"mtls_certificate_id,omitempty"`
+	Hostnames     []string `json:"hostnames"`
+}
+type ClientCertificateAuthority struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 type ClientCertificate struct {
+	Certificate          string                     `json:"certificate"`
+	CertificateAuthority ClientCertificateAuthority `json:"certificate_authority"`
+	CommonName           string                     `json:"common_name"`
+	Country              string                     `json:"country"`
+	CSR                  string                     `json:"csr"`
+	ExpiresOn            string                     `json:"expires_on"`
+	FingerprintSHA256    string                     `json:"fingerprint_sha256"`
+	ID                   string                     `json:"id"`
+	IssuedOn             string                     `json:"issued_on"`
+	Location             string                     `json:"location"`
+	Organization         string                     `json:"organization"`
+	OrganizationalUnit   string                     `json:"organizational_unit"`
+	SerialNumber         string                     `json:"serial_number"`
+	Signature            string                     `json:"signature"`
+	SKI                  string                     `json:"ski"`
+	State                string                     `json:"state"`
+	Status               string                     `json:"status"`
+	ValidityDays         int                        `json:"validity_days"`
 }
 
 type ListClientCertificatesResponse struct {
@@ -39,6 +62,11 @@ type ListClientCertificatesResponse struct {
 
 type ListClientCertificatesParams struct {
 	ResultInfo
+}
+
+type CreateClientCertificateParams struct {
+	CSR          string `json:"csr"`
+	ValidityDays int    `json:"validity_days"`
 }
 
 // ListHostnameAssociations returns a list of all domain associations for a given certificate_id.
@@ -68,7 +96,7 @@ func (api *API) ListHostnameAssociations(ctx context.Context, rc *ResourceContai
 // ReplaceHostnameAssociations associates a list of hostnames with a given certificate_id.
 //
 // API reference: https://developers.cloudflare.com/api/operations/client-certificate-for-a-zone-put-hostname-associations
-func (api *API) ReplaceHostnameAssociations(ctx context.Context, rc *ResourceContainer, params UpdateHostnameAssociationParams) (HostnameAssociation, error) {
+func (api *API) ReplaceHostnameAssociations(ctx context.Context, rc *ResourceContainer, params ReplaceHostnameAssociationParams) (HostnameAssociation, error) {
 	if rc.Level != ZoneRouteLevel {
 		return HostnameAssociation{}, ErrRequiredAccountLevelResourceContainer
 	}
@@ -136,4 +164,28 @@ func (api *API) ListClientCertificates(ctx context.Context, rc *ResourceContaine
 	}
 
 	return clientCertificates, &r.ResultInfo, nil
+}
+
+// CreateClientCertificate creates a new API Shield mTLS Client Certificate
+//
+// API reference: https://developers.cloudflare.com/api/operations/client-certificate-for-a-zone-create-client-certificate
+func (api *API) CreateClientCertificate(ctx context.Context, rc *ResourceContainer, params CreateClientCertificateParams) (ClientCertificate, error) {
+	if rc.Level != ZoneRouteLevel {
+		return ClientCertificate{}, ErrRequiredAccountLevelResourceContainer
+	}
+
+	if rc.Identifier == "" {
+		return ClientCertificate{}, ErrMissingZoneID
+	}
+
+	uri := fmt.Sprintf("/zones/%s/client_certificates", rc.Identifier)
+	res, err := api.makeRequestContext(ctx, http.MethodPost, uri, params)
+	if err != nil {
+		return ClientCertificate{}, err
+	}
+	var r ClientCertificate
+	if err := json.Unmarshal(res, &r); err != nil {
+		return ClientCertificate{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+	return r, err
 }
--- api_shield_client_certificates_test.go
+++ api_shield_client_certificates_test.go
@@ -18,6 +18,7 @@ func TestListHostnameAssociations(t *testing.T) {
 	}
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
 		w.Header().Set("content-type", "application/json")
 		fmt.Fprint(w, `{
 			"success": true,
@@ -53,6 +54,7 @@ func TestListHostnameAssociationsWithoutID(t *testing.T) {
 	params := ListHostnameAssociationParams{}
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
 		w.Header().Set("content-type", "application/json")
 		fmt.Fprint(w, `{
 			"success": true,
@@ -73,6 +75,119 @@ func TestListHostnameAssociationsWithoutID(t *testing.T) {
 	}
 
 	actual, err := client.ListHostnameAssociations(context.Background(), ZoneIdentifier(testZoneID), params)
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestReplaceHostnameAssociations(t *testing.T) {
+	setup()
+	defer teardown()
+
+	params := ReplaceHostnameAssociationParams{
+		CertificateID: "52ad7c3e-f37e-4835-8b24-07d8afcaeb3",
+		Hostnames:     []string{"test.example.com"},
+	}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": {
+				"mtls_certificate_id": "52ad7c3e-f37e-4835-8b24-07d8afcaeb3",
+				"hostnames": [
+					"test.example.com"
+				]
+			}
+		}`)
+	}
+
+	mux.HandleFunc("/zones/"+testZoneID+"/certificate_authorities/hostname_associations", handler)
+
+	want := HostnameAssociation{
+		CertificateID: "52ad7c3e-f37e-4835-8b24-07d8afcaeb3",
+		Hostnames:     []string{"test.example.com"},
+	}
+
+	actual, err := client.ReplaceHostnameAssociations(context.Background(), ZoneIdentifier(testZoneID), params)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, actual)
+	}
+}
+
+func TestListClientCertificates(t *testing.T) {
+	setup()
+	defer teardown()
+
+	params := ListClientCertificatesParams{}
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+			"success": true,
+			"errors": [],
+			"messages": [],
+			"result": [{
+				"certificate": "-----BEGIN CERTIFICATE-----\nMIIDmDCCAoC...dhDDE\n-----END CERTIFICATE-----",
+				"certificate_authority": {
+					"id": "568b6b74-7b0c-4755-8840-4e3b8c24adeb",
+					"name": "Cloudflare Managed CA for account"
+				},
+				"common_name": "Cloudflare",
+				"country": "US",
+				"csr": "-----BEGIN CERTIFICATE REQUEST-----\nMIICY....\n-----END CERTIFICATE REQUEST-----\n",
+				"expires_on": "2033-02-20T23:18:00Z",
+				"fingerprint_sha256": "256c24690243359fb8cf139a125bd05ebf1d968b71e4caf330718e9f5c8a89ea",
+				"id": "023e105f4ecef8ad9ca31a8372d0c353",
+				"issued_on": "2023-02-23T23:18:00Z",
+				"location": "Somewhere",
+				"organization": "Organization",
+				"organizational_unit": "Organizational Unit",
+				"serial_number": "3bb94ff144ac567b9f75ad664b6c55f8d5e48182",
+				"signature": "SHA256WithRSA",
+				"ski": "8e375af1389a069a0f921f8cc8e1eb12d784b949",
+				"state": "CA",
+				"status": "active",
+				"validity_days": 3650
+			}]
+		}`)
+	}
+
+	mux.HandleFunc("/zones/"+testZoneID+"/client_certificates", handler)
+
+	want := []ClientCertificate{
+		{
+			Certificate: "-----BEGIN CERTIFICATE-----\nMIIDmDCCAoC...dhDDE\n-----END CERTIFICATE-----",
+			CertificateAuthority: ClientCertificateAuthority{
+				ID:   "568b6b74-7b0c-4755-8840-4e3b8c24adeb",
+				Name: "Cloudflare Managed CA for account",
+			},
+			CommonName:         "Cloudflare",
+			Country:            "US",
+			CSR:                "-----BEGIN CERTIFICATE REQUEST-----\nMIICY....\n-----END CERTIFICATE REQUEST-----\n",
+			ExpiresOn:          "2033-02-20T23:18:00Z",
+			FingerprintSHA256:  "256c24690243359fb8cf139a125bd05ebf1d968b71e4caf330718e9f5c8a89ea",
+			ID:                 "023e105f4ecef8ad9ca31a8372d0c353",
+			IssuedOn:           "2023-02-23T23:18:00Z",
+			Location:           "Somewhere",
+			Organization:       "Organization",
+			OrganizationalUnit: "Organizational Unit",
+			SerialNumber:       "3bb94ff144ac567b9f75ad664b6c55f8d5e48182",
+			Signature:          "SHA256WithRSA",
+			SKI:                "8e375af1389a069a0f921f8cc8e1eb12d784b949",
+			State:              "CA",
+			Status:             "active",
+			ValidityDays:       3650,
+		},
+	}
+
+	actual, _, err := client.ListClientCertificates(context.Background(), ZoneIdentifier(testZoneID), params)
+
 	if assert.NoError(t, err) {
 		assert.Equal(t, want, actual)
 	}
