bb420fe58a5d095e20cbf2383c84ff514a7b738e
Sai Dadireddy <sdadireddy@cloudflare.com>
2024-09-19 09:02:12 -0700

    Add CRUD endpoints for infrastructure access endpoints

---
+++ infrastructure_targets.go
@@ -0,0 +1,236 @@
+package cloudflare
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/goccy/go-json"
+)
+
+var ErrMissingTargetId = errors.New("required target id missing")
+
+// AccessApplication represents an Access application.
+type Target struct {
+	Hostname   string    `json:"hostname"`
+	ID         string    `json:"id"`
+	IP         IPInfo    `json:"ip"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
+}
+
+type IPDetails struct {
+	IpAddr           string `json:"ip_addr"`
+	VirtualNetworkId string `json:"virtual_network_id"`
+}
+
+type IPInfo struct {
+	IPV4 *IPDetails `json:"ipv4,omitempty"`
+	IPV6 *IPDetails `json:"ipv6,omitempty"`
+}
+
+type InfrastructureTargetParams struct {
+	Hostname string `json:"hostname"`
+	IP       IPInfo `json:"ip"`
+}
+
+type CreateInfrastructureTargetParams struct {
+	InfrastructureTargetParams
+}
+
+type UpdateInfrastructureTargetParams struct {
+	ID           string                     `json:"hostname"`
+	ModifyParams InfrastructureTargetParams `json:"modify_params"`
+}
+
+// TargetDetailResponse is the API response, containing a single access target.
+type TargetDetailResponse struct {
+	Result Target `json:"result"`
+	Response
+}
+
+type TargetListDetailResponse struct {
+	Result []Target `json:"result"`
+	Response
+	ResultInfo `json:"result_info"`
+}
+
+type TargetListParams struct {
+	CreatedAfter     string `url:"created_after,omitempty"`
+	Hostname         string `url:"hostname,omitempty"`
+	HostnameContains string `url:"hostname_contains,omitempty"`
+	IPV4             string `url:"ip_v4,omitempty"`
+	IPV6             string `url:"ip_v6,omitempty"`
+	ModifedAfter     string `url:"modified_after,omitempty"`
+	VirtualNetworkId string `url:"virtual_network_id,omitempty"`
+
+	ResultInfo
+}
+
+// ListInfrastructureTargets returns all applications within an account or zone.
+//
+// Account API reference: https://developers.cloudflare.com/api/operations/infra-targets-list
+func (api *API) ListInfrastructureTargets(ctx context.Context, rc *ResourceContainer, params TargetListParams) ([]Target, *ResultInfo, error) {
+	if rc.Identifier == "" {
+		return []Target{}, &ResultInfo{}, ErrMissingAccountID
+	}
+
+	baseURL := fmt.Sprintf("/%s/%s/infrastructure/targets", rc.Level, rc.Identifier)
+
+	autoPaginate := true
+	if params.PerPage >= 1 || params.Page >= 1 {
+		autoPaginate = false
+	}
+
+	if params.PerPage < 1 {
+		params.PerPage = 25
+	}
+
+	if params.Page < 1 {
+		params.Page = 1
+	}
+
+	var applications []Target
+	var r TargetListDetailResponse
+
+	for {
+		uri := buildURI(baseURL, params)
+
+		res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+		if err != nil {
+			return []Target{}, &ResultInfo{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+		}
+
+		err = json.Unmarshal(res, &r)
+		if err != nil {
+			return []Target{}, &ResultInfo{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+		}
+		applications = append(applications, r.Result...)
+		params.ResultInfo = r.ResultInfo.Next()
+		if params.ResultInfo.Done() || !autoPaginate {
+			break
+		}
+	}
+
+	return applications, &r.ResultInfo, nil
+}
+
+// CreateInfrastructureTarget creates a new infrastructure target.
+//
+// Account API reference: https://developers.cloudflare.com/api/operations/infra-targets-post
+func (api *API) CreateInfrastructureTarget(ctx context.Context, rc *ResourceContainer, params CreateInfrastructureTargetParams) (Target, error) {
+	if rc.Identifier == "" {
+		return Target{}, ErrMissingAccountID
+	}
+
+	uri := fmt.Sprintf("/%s/%s/infrastructure/targets", rc.Level, rc.Identifier)
+
+	res, err := api.makeRequestContext(ctx, http.MethodPost, uri, params)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+
+	var targetDetailResponse TargetDetailResponse
+	err = json.Unmarshal(res, &targetDetailResponse)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return targetDetailResponse.Result, nil
+}
+
+// UpdateInfrastructureTarget updates an existing infrastructure target.
+//
+// Account API reference: https://developers.cloudflare.com/api/operations/infra-targets-put
+func (api *API) UpdateInfrastructureTarget(ctx context.Context, rc *ResourceContainer, params UpdateInfrastructureTargetParams) (Target, error) {
+	if rc.Identifier == "" {
+		return Target{}, ErrMissingAccountID
+	}
+
+	if params.ID == "" {
+		return Target{}, ErrMissingTargetId
+	}
+
+	uri := fmt.Sprintf(
+		"/%s/%s/infrastructure/targets/%s",
+		rc.Level,
+		rc.Identifier,
+		params.ID,
+	)
+
+	res, err := api.makeRequestContext(ctx, http.MethodPut, uri, params.ModifyParams)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+
+	var targetDetailResponse TargetDetailResponse
+	err = json.Unmarshal(res, &targetDetailResponse)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return targetDetailResponse.Result, nil
+}
+
+// GetInfrastructureTarget returns a single infrastructure target based on target ID
+// ID for either account or zone.
+//
+// Account API reference: https://developers.cloudflare.com/api/operations/infra-targets-get
+func (api *API) GetInfrastructureTarget(ctx context.Context, rc *ResourceContainer, targetID string) (Target, error) {
+	if rc.Identifier == "" {
+		return Target{}, ErrMissingAccountID
+	}
+
+	if targetID == "" {
+		return Target{}, ErrMissingTargetId
+	}
+
+	uri := fmt.Sprintf(
+		"/%s/%s/infrastructure/targets/%s",
+		rc.Level,
+		rc.Identifier,
+		targetID,
+	)
+
+	res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+
+	var targetDetailResponse TargetDetailResponse
+	err = json.Unmarshal(res, &targetDetailResponse)
+	if err != nil {
+		return Target{}, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return targetDetailResponse.Result, nil
+}
+
+// DeleteInfrastructureTarget deletes an infrastructure target.
+//
+// Account API reference: https://developers.cloudflare.com/api/operations/infra-targets-delete
+func (api *API) DeleteInfrastructureTarget(ctx context.Context, rc *ResourceContainer, targetID string) error {
+	if rc.Identifier == "" {
+		return ErrMissingAccountID
+	}
+
+	if targetID == "" {
+		return ErrMissingTargetId
+	}
+
+	uri := fmt.Sprintf(
+		"/%s/%s/infrastructure/targets/%s",
+		rc.Level,
+		rc.Identifier,
+		targetID,
+	)
+
+	_, err := api.makeRequestContext(ctx, http.MethodDelete, uri, nil)
+	if err != nil {
+		return fmt.Errorf("%s: %w", errMakeRequestError, err)
+	}
+
+	return nil
+}
---
+++ infrastructure_targets_test.go
@@ -0,0 +1,287 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// randomly generated uuid
+const testInfrastructureTargetId = "019205b5-97d7-7272-b00e-0ea05e61a124"
+
+var (
+	infrastrctureTargetCreatedOn, _  = time.Parse(time.RFC3339, "2024-08-25T05:00:22Z")
+	infrastrctureTargetModifiedOn, _ = time.Parse(time.RFC3339, "2024-08-25T05:00:22Z")
+	expectedInfrastructureTarget     = Target{
+		Hostname: "infra-access-target",
+		ID:       testInfrastructureTargetId,
+		IP: IPInfo{
+			IPV4: &IPDetails{
+				IpAddr:           "187.26.29.249",
+				VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+			},
+			IPV6: &IPDetails{
+				IpAddr:           "64c0:64e8:f0b4:8dbf:7104:72b0:ec8f:f5e0",
+				VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+			},
+		},
+		CreatedAt:  infrastrctureTargetCreatedOn,
+		ModifiedAt: infrastrctureTargetModifiedOn,
+	}
+	expectedInfrastructureModified = Target{
+		Hostname: "infra-access-target-modified",
+		ID:       testInfrastructureTargetId,
+		IP: IPInfo{
+			IPV4: &IPDetails{
+				IpAddr:           "250.26.29.250",
+				VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+			},
+		},
+		CreatedAt:  infrastrctureTargetCreatedOn,
+		ModifiedAt: infrastrctureTargetModifiedOn,
+	}
+)
+
+func TestInfrastructureTarget_Create(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/infrastructure/targets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPost, r.Method, "Expected method 'POST', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+			{
+			  "success": true,
+			  "errors": [],
+			  "messages": [],
+			  "result": {
+			 	"created_at": "2024-08-25T05:00:22Z",
+				"hostname": "infra-access-target",
+				"id": "019205b5-97d7-7272-b00e-0ea05e61a124",
+				"ip": {
+				"ipv4": {
+					"ip_addr": "187.26.29.249",
+					"virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+				},
+				"ipv6": {
+					"ip_addr": "64c0:64e8:f0b4:8dbf:7104:72b0:ec8f:f5e0",
+					"virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+				}
+				},
+				"modified_at": "2024-08-25T05:00:22Z"
+			  }
+			}`)
+	})
+
+	// Make sure missing account ID is thrown
+	_, err := client.CreateInfrastructureTarget(context.Background(), AccountIdentifier(""), CreateInfrastructureTargetParams{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	out, err := client.CreateInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), CreateInfrastructureTargetParams{
+		InfrastructureTargetParams: InfrastructureTargetParams{
+			Hostname: "infra-access-target",
+			IP: IPInfo{
+				IPV4: &IPDetails{
+					IpAddr:           "187.26.29.249",
+					VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+				},
+				IPV6: &IPDetails{
+					IpAddr:           "64c0:64e8:f0b4:8dbf:7104:72b0:ec8f:f5e0",
+					VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+				},
+			},
+		},
+	})
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedInfrastructureTarget, out, "create infrastructure_target structs not equal")
+	}
+}
+
+func TestInfrastructureTarget_List(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/infrastructure/targets", func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": [
+    {
+	  "created_at": "2024-08-25T05:00:22Z",
+	  "hostname": "infra-access-target",
+	  "id": "019205b5-97d7-7272-b00e-0ea05e61a124",
+	  "ip": {
+	  	"ipv4": {
+		  "ip_addr": "187.26.29.249",
+		  "virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+		},
+		"ipv6": {
+		  "ip_addr": "64c0:64e8:f0b4:8dbf:7104:72b0:ec8f:f5e0",
+		  "virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+		}
+	  },
+	  "modified_at": "2024-08-25T05:00:22Z"
+    }
+  ],
+  "result_info": {
+    "page": 1,
+    "per_page": 20,
+    "count": 1,
+    "total_count": 1
+  }
+}`)
+	})
+
+	_, _, err := client.ListInfrastructureTargets(context.Background(), AccountIdentifier(""), TargetListParams{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	out, results, err := client.ListInfrastructureTargets(context.Background(), AccountIdentifier(testAccountID), TargetListParams{})
+	if assert.NoError(t, err) {
+		assert.Equal(t, 1, len(out), "expected 1 challenge_widgets")
+		assert.Equal(t, 20, results.PerPage, "expected 20 per page")
+		assert.Equal(t, expectedInfrastructureTarget, out[0], "list challenge_widgets structs not equal")
+	}
+}
+
+func TestInfrastructureTarget_Get(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/infrastructure/targets/"+testInfrastructureTargetId, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+	"created_at": "2024-08-25T05:00:22Z",
+    "hostname": "infra-access-target",
+    "id": "019205b5-97d7-7272-b00e-0ea05e61a124",
+    "ip": {
+      "ipv4": {
+        "ip_addr": "187.26.29.249",
+        "virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+      },
+      "ipv6": {
+        "ip_addr": "64c0:64e8:f0b4:8dbf:7104:72b0:ec8f:f5e0",
+        "virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+      }
+    },
+    "modified_at": "2024-08-25T05:00:22Z"
+  }
+}`)
+	})
+
+	_, err := client.GetInfrastructureTarget(context.Background(), AccountIdentifier(""), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	_, err = client.GetInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingTargetId, err)
+	}
+
+	out, err := client.GetInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), testInfrastructureTargetId)
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedInfrastructureTarget, out, "get infrastructure_target not equal to expected")
+	}
+}
+
+func TestInfrastructureTarget_Update(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/infrastructure/targets/"+testInfrastructureTargetId, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `
+{
+  "success": true,
+  "errors": [],
+  "messages": [],
+  "result": {
+	"created_at": "2024-08-25T05:00:22Z",
+    "hostname": "infra-access-target-modified",
+    "id": "019205b5-97d7-7272-b00e-0ea05e61a124",
+    "ip": {
+      "ipv4": {
+        "ip_addr": "250.26.29.250",
+        "virtual_network_id": "c77b744e-acc8-428f-9257-6878c046ed55"
+      }
+    },
+    "modified_at": "2024-08-25T05:00:22Z"
+  }
+}`)
+	})
+
+	_, err := client.UpdateInfrastructureTarget(context.Background(), AccountIdentifier(""), UpdateInfrastructureTargetParams{})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	_, err = client.UpdateInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), UpdateInfrastructureTargetParams{
+		ID:           "",
+		ModifyParams: InfrastructureTargetParams{},
+	})
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingTargetId, err)
+	}
+
+	out, err := client.UpdateInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), UpdateInfrastructureTargetParams{
+		ID: testInfrastructureTargetId,
+		ModifyParams: InfrastructureTargetParams{
+			// Updates hostname and IPv4 address. Deletes IPv6 address.
+			Hostname: "infra-access-target-modified",
+			IP: IPInfo{
+				IPV4: &IPDetails{
+					IpAddr:           "250.26.29.250",
+					VirtualNetworkId: "c77b744e-acc8-428f-9257-6878c046ed55",
+				},
+			},
+		},
+	})
+	if assert.NoError(t, err) {
+		assert.Equal(t, expectedInfrastructureModified, out, "update challenge_widgets structs not equal")
+	}
+}
+
+func TestInfrastructureTarget_Delete(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/accounts/"+testAccountID+"/infrastructure/targets/"+testInfrastructureTargetId, func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodDelete, r.Method, "Expected method 'DELETE', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, ``)
+	})
+
+	// Make sure missing account ID is thrown
+	err := client.DeleteInfrastructureTarget(context.Background(), AccountIdentifier(""), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingAccountID, err)
+	}
+
+	err = client.DeleteInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), "")
+	if assert.Error(t, err) {
+		assert.Equal(t, ErrMissingTargetId, err)
+	}
+
+	err = client.DeleteInfrastructureTarget(context.Background(), AccountIdentifier(testAccountID), testInfrastructureTargetId)
+	assert.NoError(t, err)
+}
