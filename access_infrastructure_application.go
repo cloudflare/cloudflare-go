76fa2f79d1f3d13d8ad69145485237656febcd82
Sai Dadireddy <sdadireddy@cloudflare.com>
2024-09-25 15:12:40 -0700

    Cleanup comments

--- access_infrastructure_application.go
+++ access_infrastructure_application.go
@@ -7,8 +7,6 @@ const (
 	RDP      InfraProtocol = "RDP"
 )
 
-// WARNING : The following table has a constraint. Each organization can specify at maximum one protocol for each of their ports.
-// There can be at maximum a single (OrgID, Port, Protocol) combination, which can be shared across multiple records.
 type InfraTargetContext struct {
 	TargetAttributes map[string][]string `json:"target_attributes"`
 	Port             int                 `json:"port"`
