f62fd6ac5ea61bc0c78b8df5487760960b2b991c
Denis Davydov <ddavydov@cloudflare.com>
2024-10-21 17:01:07 +0100

    FLPROD-796: Create snippets api client

---
+++ snippets_rules.go
@@ -0,0 +1,66 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/goccy/go-json"
+)
+
+type SnippetsRulesResponse struct {
+	Response
+	Result []SnippetRule `json:"result"`
+}
+
+type SnippetRule struct {
+	ID          string `json:"id"`
+	Enabled     *bool  `json:"enabled,omitempty"`
+	Expression  string `json:"expression"`
+	SnippetName string `json:"snippet_name"`
+	Description string `json:"description"`
+}
+
+func (api *API) ListZoneSnippetsRules(ctx context.Context, rc *ResourceContainer) ([]SnippetRule, error) {
+	if rc.Identifier == "" {
+		return nil, ErrMissingZoneID
+	}
+
+	uri := buildURI(fmt.Sprintf("/zones/%s/snippets/rules", rc.Identifier), nil)
+	res, err := api.makeRequestContext(ctx, http.MethodGet, uri, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	result := SnippetsRulesResponse{}
+	if err := json.Unmarshal(res, &result); err != nil {
+		return nil, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return result.Result, nil
+}
+
+func (api *API) UpdateZoneSnippetsRules(ctx context.Context, rc *ResourceContainer, params []SnippetRule) ([]SnippetRule, error) {
+	if rc.Identifier == "" {
+		return nil, ErrMissingZoneID
+	}
+
+	uri := fmt.Sprintf("/zones/%s/snippets/rules", rc.Identifier)
+
+	payload, err := json.Marshal(params)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := api.makeRequestContext(ctx, http.MethodPut, uri, payload)
+	if err != nil {
+		return nil, err
+	}
+
+	result := SnippetsRulesResponse{}
+	if err := json.Unmarshal(res, &result); err != nil {
+		return nil, fmt.Errorf("%s: %w", errUnmarshalError, err)
+	}
+
+	return result.Result, nil
+}
---
+++ snippets_test.go
@@ -0,0 +1,116 @@
+package cloudflare
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSnippets(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodGet, r.Method, "Expected method 'GET', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+      "result": [
+		{
+			"snippet_name": "some_id_1",
+			"created_on":"0001-01-01T00:00:00Z",
+			"modified_on":"0001-01-01T00:00:00Z"
+	  	},
+		{
+			"snippet_name": "some_id_2",
+			"created_on":"0001-01-01T00:00:00Z",
+			"modified_on":"0001-01-01T00:00:00Z"
+	  	}
+	  ],
+      "success": true,
+      "errors": [],
+      "messages": []
+    }`)
+	}
+	mux.HandleFunc("/zones/"+testZoneID+"/snippets", handler)
+
+	want := []Snippet{
+		{
+			SnippetName: "some_id_1",
+			CreatedOn:   time.Time{},
+			ModifiedOn:  time.Time{},
+		},
+		{
+			SnippetName: "some_id_2",
+			CreatedOn:   time.Time{},
+			ModifiedOn:  time.Time{},
+		},
+	}
+
+	zoneActual, err := client.ListZoneSnippets(context.Background(), ZoneIdentifier(testZoneID))
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, zoneActual)
+	}
+}
+
+func TestUpdateSnippets(t *testing.T) {
+	setup()
+	defer teardown()
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, http.MethodPut, r.Method, "Expected method 'PUT', got %s", r.Method)
+		w.Header().Set("content-type", "application/json")
+		fmt.Fprint(w, `{
+      "result": [
+		{
+			"snippet_name": "some_id_1",
+			"created_on":"0001-01-01T00:00:00Z",
+			"modified_on":"0001-01-01T00:00:00Z"
+	  	},
+		{
+			"snippet_name": "some_id_2",
+			"created_on":"0001-01-01T00:00:00Z",
+			"modified_on":"0001-01-01T00:00:00Z"
+	  	}
+	  ],
+      "success": true,
+      "errors": [],
+      "messages": []
+    }`)
+	}
+
+	mux.HandleFunc("/zones/"+testZoneID+"/snippets/some_id_1", handler)
+	toUpdate := SnippetRequest{
+		SnippetName: "some_id_1",
+		MainFile:    "file1.js",
+		Files: []SnippetFile{
+			{
+				FileName: "file1.js",
+				Content:  "test1.json",
+			},
+			{
+				FileName: "file2.js",
+				Content:  "test2.json",
+			},
+		},
+	}
+	want := []Snippet{
+		{
+			SnippetName: "some_id_1",
+			CreatedOn:   time.Time{},
+			ModifiedOn:  time.Time{},
+		},
+		{
+			SnippetName: "some_id_2",
+			CreatedOn:   time.Time{},
+			ModifiedOn:  time.Time{},
+		},
+	}
+	zoneActual, err := client.UpdateZoneSnippet(context.Background(), ZoneIdentifier(testZoneID), toUpdate)
+	if assert.NoError(t, err) {
+		assert.Equal(t, want, zoneActual)
+	}
+}
