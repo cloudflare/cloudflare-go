a4693433c367d452530f863e7b8000d3380df0d6
Eric Bissonnette <ebisso.dev@gmail.com>
2024-12-13 19:19:28 -0500

    types

--- certificate_authorities.go
+++ certificate_authorities.go
@@ -3,12 +3,12 @@ package cloudflare
 import "context"
 
 type ListCertificateAuthoritiesHostnameAssociationsParams struct {
-	MTLSCertificateID *string `url:"mtls_certificate_id,omitempty"`
+	MTLSCertificateID string `url:"mtls_certificate_id,omitempty"`
 }
 
 type UpdateCertificateAuthoritiesHostnameAssociationsParams struct {
 	Hostnames         []HostnameAssociation `json:"hostnames,omitempty"`
-	MTLSCertificateID *string               `json:"mtls_certificate_id,omitempty"`
+	MTLSCertificateID string                `json:"mtls_certificate_id,omitempty"`
 }
 
 type HostnameAssociation = string
@@ -16,13 +16,13 @@ type HostnameAssociation = string
 // List Hostname Associations
 //
 // API Reference: https://developers.cloudflare.com/api/operations/client-certificate-for-a-zone-list-hostname-associations
-func (api *API) ListCertificateAuthoritiesHostnameAssociations(ctx context.Context, rc *ResourceContainer, params ListCertificateAuthoritiesHostnameAssociationsParams) ([]string, error) {
-	return []string{}, nil
+func (api *API) ListCertificateAuthoritiesHostnameAssociations(ctx context.Context, rc *ResourceContainer, params ListCertificateAuthoritiesHostnameAssociationsParams) ([]HostnameAssociation, error) {
+	return []HostnameAssociation{}, nil
 }
 
 // Replace Hostname Associations
 //
 // API Reference: https://developers.cloudflare.com/api/operations/client-certificate-for-a-zone-put-hostname-associations
-func (api *API) UpdateCertificateAuthoritiesHostnameAssociations(ctx context.Context, rc *ResourceContainer, params UpdateCertificateAuthoritiesHostnameAssociationsParams) ([]string, error) {
-	return []string{}, nil
+func (api *API) UpdateCertificateAuthoritiesHostnameAssociations(ctx context.Context, rc *ResourceContainer, params UpdateCertificateAuthoritiesHostnameAssociationsParams) ([]HostnameAssociation, error) {
+	return []HostnameAssociation{}, nil
 }
