1cb4b2f2279633bc77f5e1da20ec37f93edd89ed
Andrew Hay <39sumer3939@gmail.com>
2025-02-19 17:59:11 +0000

    test(regionalhostnameregion): isValid

--- addressing/regionalhostnameregion_test.go
+++ addressing/regionalhostnameregion_test.go
@@ -38,3 +38,40 @@ func TestRegionalHostnameRegionList(t *testing.T) {
 		t.Fatalf("err should be nil: %s", err.Error())
 	}
 }
+
+func TestRegionalHostnameRegionValidate(t *testing.T) {
+	baseURL := "http://localhost:4010"
+	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
+		baseURL = envURL
+	}
+	if !testutil.CheckTestServer(t, baseURL) {
+		return
+	}
+	client := cloudflare.NewClient(
+		option.WithBaseURL(baseURL),
+		option.WithAPIKey("144c9defac04969c7bfad8efaa8ea194"),
+		option.WithAPIEmail("user@example.com"),
+	)
+
+	// Test valid region
+	isValid, err := client.Addressing.RegionalHostnames.Regions.Validate(context.TODO(), addressing.RegionalHostnameRegionListParams{
+		AccountID: cloudflare.F("023e105f4ecef8ad9ca31a8372d0c353"),
+	})
+	if err != nil {
+		t.Fatalf("err should be nil: %s", err.Error())
+	}
+	if !isValid {
+		t.Errorf("isValid should be true")
+	}
+
+	// Test invalid region
+	isValid, err = client.Addressing.RegionalHostnames.Regions.Validate(context.TODO(), addressing.RegionalHostnameRegionListParams{
+		AccountID: cloudflare.F(" invalid-region"),
+	})
+	if err != nil {
+		t.Fatalf("err should be nil: %s", err.Error())
+	}
+	if isValid {
+		t.Errorf("isValid should be false")
+	}
+}
