08a1e8cd0f7051ee5a5c8b70688ad3ee39db6959
Burny Lu <blu@canva.com>
2026-02-15 07:54:59 +1100

    Apply suggestion from @burnster

--- zero_trust/accessservicetoken.go
+++ zero_trust/accessservicetoken.go
@@ -270,7 +270,6 @@ type ServiceToken struct {
 	// default is 1 year in hours (8760h).
 	Duration  string    `json:"duration"`
 	ExpiresAt time.Time `json:"expires_at" format:"date-time"`
-	// The date and time the service token was last used.
 	LastSeenAt time.Time `json:"last_seen_at" format:"date-time"`
 	// The name of the service token.
 	Name string           `json:"name"`
