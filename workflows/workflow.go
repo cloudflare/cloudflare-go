76b65c5404a1e8838a848409f39759445d0619fe
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-16 14:56:47 -0700

    chore(workflows): update generated types and methods

--- workflows/workflow.go
+++ workflows/workflow.go
@@ -46,6 +46,11 @@ func NewWorkflowService(opts ...option.RequestOption) (r *WorkflowService) {
 func (r *WorkflowService) Update(ctx context.Context, workflowName string, params WorkflowUpdateParams, opts ...option.RequestOption) (res *WorkflowUpdateResponse, err error) {
 	var env WorkflowUpdateResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -68,6 +73,11 @@ func (r *WorkflowService) List(ctx context.Context, params WorkflowListParams, o
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -95,6 +105,11 @@ func (r *WorkflowService) ListAutoPaging(ctx context.Context, params WorkflowLis
 func (r *WorkflowService) Delete(ctx context.Context, workflowName string, body WorkflowDeleteParams, opts ...option.RequestOption) (res *WorkflowDeleteResponse, err error) {
 	var env WorkflowDeleteResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&body.AccountID, precfg.AccountID)
 	if body.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -116,6 +131,11 @@ func (r *WorkflowService) Delete(ctx context.Context, workflowName string, body 
 func (r *WorkflowService) Get(ctx context.Context, workflowName string, query WorkflowGetParams, opts ...option.RequestOption) (res *WorkflowGetResponse, err error) {
 	var env WorkflowGetResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.AccountID, precfg.AccountID)
 	if query.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -350,6 +370,7 @@ func (r workflowGetResponseInstancesJSON) RawJSON() string {
 }
 
 type WorkflowUpdateParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID  param.Field[string]                     `path:"account_id" api:"required"`
 	ClassName  param.Field[string]                     `json:"class_name" api:"required"`
 	ScriptName param.Field[string]                     `json:"script_name" api:"required"`
@@ -463,6 +484,7 @@ type WorkflowUpdateResponseEnvelopeResultInfo struct {
 	TotalCount float64                                      `json:"total_count" api:"required"`
 	Cursor     string                                       `json:"cursor"`
 	Page       float64                                      `json:"page"`
+	TotalPages float64                                      `json:"total_pages"`
 	JSON       workflowUpdateResponseEnvelopeResultInfoJSON `json:"-"`
 }
 
@@ -474,6 +496,7 @@ type workflowUpdateResponseEnvelopeResultInfoJSON struct {
 	TotalCount  apijson.Field
 	Cursor      apijson.Field
 	Page        apijson.Field
+	TotalPages  apijson.Field
 	raw         string
 	ExtraFields map[string]apijson.Field
 }
@@ -487,6 +510,7 @@ func (r workflowUpdateResponseEnvelopeResultInfoJSON) RawJSON() string {
 }
 
 type WorkflowListParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string]  `path:"account_id" api:"required"`
 	Page      param.Field[float64] `query:"page"`
 	PerPage   param.Field[float64] `query:"per_page"`
@@ -503,6 +527,7 @@ func (r WorkflowListParams) URLQuery() (v url.Values) {
 }
 
 type WorkflowDeleteParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
@@ -601,6 +626,7 @@ type WorkflowDeleteResponseEnvelopeResultInfo struct {
 	TotalCount float64                                      `json:"total_count" api:"required"`
 	Cursor     string                                       `json:"cursor"`
 	Page       float64                                      `json:"page"`
+	TotalPages float64                                      `json:"total_pages"`
 	JSON       workflowDeleteResponseEnvelopeResultInfoJSON `json:"-"`
 }
 
@@ -612,6 +638,7 @@ type workflowDeleteResponseEnvelopeResultInfoJSON struct {
 	TotalCount  apijson.Field
 	Cursor      apijson.Field
 	Page        apijson.Field
+	TotalPages  apijson.Field
 	raw         string
 	ExtraFields map[string]apijson.Field
 }
@@ -625,6 +652,7 @@ func (r workflowDeleteResponseEnvelopeResultInfoJSON) RawJSON() string {
 }
 
 type WorkflowGetParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
@@ -723,6 +751,7 @@ type WorkflowGetResponseEnvelopeResultInfo struct {
 	TotalCount float64                                   `json:"total_count" api:"required"`
 	Cursor     string                                    `json:"cursor"`
 	Page       float64                                   `json:"page"`
+	TotalPages float64                                   `json:"total_pages"`
 	JSON       workflowGetResponseEnvelopeResultInfoJSON `json:"-"`
 }
 
@@ -734,6 +763,7 @@ type workflowGetResponseEnvelopeResultInfoJSON struct {
 	TotalCount  apijson.Field
 	Cursor      apijson.Field
 	Page        apijson.Field
+	TotalPages  apijson.Field
 	raw         string
 	ExtraFields map[string]apijson.Field
 }
