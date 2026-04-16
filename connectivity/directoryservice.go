9a230873597c1f3940833761c253d53dc8ba2b5e
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-16 14:53:08 -0700

    chore(connectivity): update generated types and methods

--- connectivity/directoryservice.go
+++ connectivity/directoryservice.go
@@ -44,6 +44,11 @@ func NewDirectoryServiceService(opts ...option.RequestOption) (r *DirectoryServi
 func (r *DirectoryServiceService) New(ctx context.Context, params DirectoryServiceNewParams, opts ...option.RequestOption) (res *DirectoryServiceNewResponse, err error) {
 	var env DirectoryServiceNewResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -61,6 +66,11 @@ func (r *DirectoryServiceService) New(ctx context.Context, params DirectoryServi
 func (r *DirectoryServiceService) Update(ctx context.Context, serviceID string, params DirectoryServiceUpdateParams, opts ...option.RequestOption) (res *DirectoryServiceUpdateResponse, err error) {
 	var env DirectoryServiceUpdateResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -83,6 +93,11 @@ func (r *DirectoryServiceService) List(ctx context.Context, params DirectoryServ
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -109,6 +124,11 @@ func (r *DirectoryServiceService) ListAutoPaging(ctx context.Context, params Dir
 func (r *DirectoryServiceService) Delete(ctx context.Context, serviceID string, body DirectoryServiceDeleteParams, opts ...option.RequestOption) (err error) {
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithHeader("Accept", "*/*")}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return err
+	}
+	requestconfig.UseDefaultParam(&body.AccountID, precfg.AccountID)
 	if body.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return err
@@ -126,6 +146,11 @@ func (r *DirectoryServiceService) Delete(ctx context.Context, serviceID string, 
 func (r *DirectoryServiceService) Get(ctx context.Context, serviceID string, query DirectoryServiceGetParams, opts ...option.RequestOption) (res *DirectoryServiceGetResponse, err error) {
 	var env DirectoryServiceGetResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.AccountID, precfg.AccountID)
 	if query.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -3691,6 +3716,8 @@ func (r DirectoryServiceGetResponseAppProtocol) IsKnown() bool {
 
 type DirectoryServiceNewParams struct {
 	// Account identifier
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string]                `path:"account_id" api:"required"`
 	Body      DirectoryServiceNewParamsBodyUnion `json:"body" api:"required"`
 }
@@ -4222,6 +4249,7 @@ func (r DirectoryServiceNewResponseEnvelopeSuccess) IsKnown() bool {
 }
 
 type DirectoryServiceUpdateParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string]                   `path:"account_id" api:"required"`
 	Body      DirectoryServiceUpdateParamsBodyUnion `json:"body" api:"required"`
 }
@@ -4755,6 +4783,8 @@ func (r DirectoryServiceUpdateResponseEnvelopeSuccess) IsKnown() bool {
 
 type DirectoryServiceListParams struct {
 	// Account identifier
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 	// Current page in the response
 	Page param.Field[int64] `query:"page"`
@@ -4788,10 +4818,12 @@ func (r DirectoryServiceListParamsType) IsKnown() bool {
 }
 
 type DirectoryServiceDeleteParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
 type DirectoryServiceGetParams struct {
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
