df36b3c9fd453051172266e3fe7f494aab175e9d
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-16 14:55:43 -0700

    chore(resource_tagging): update generated types and methods

--- resource_tagging/value.go
+++ resource_tagging/value.go
@@ -42,6 +42,11 @@ func (r *ValueService) List(ctx context.Context, tagKey string, params ValueList
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -71,6 +76,8 @@ func (r *ValueService) ListAutoPaging(ctx context.Context, tagKey string, params
 
 type ValueListParams struct {
 	// Identifier.
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 	// Cursor for pagination.
 	Cursor param.Field[string] `query:"cursor"`
