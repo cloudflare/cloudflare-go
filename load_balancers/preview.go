c266a5a09ad4960b9211cfbb0ab07dfd02c3b0dc
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 10:56:53 -0700

    chore(load_balancers): update generated types and methods

--- load_balancers/preview.go
+++ load_balancers/preview.go
@@ -39,6 +39,11 @@ func NewPreviewService(opts ...option.RequestOption) (r *PreviewService) {
 func (r *PreviewService) Get(ctx context.Context, previewID string, query PreviewGetParams, opts ...option.RequestOption) (res *PreviewGetResponse, err error) {
 	var env PreviewGetResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.AccountID, precfg.AccountID)
 	if query.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -111,6 +116,8 @@ func (r previewGetResponseItemOriginJSON) RawJSON() string {
 
 type PreviewGetParams struct {
 	// Identifier.
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
