5d3458387fff39b1b6f720d81d4b18684b26d978
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 09:58:18 -0700

    chore(url_scanner): update generated types and methods

--- url_scanner/response.go
+++ url_scanner/response.go
@@ -38,6 +38,11 @@ func NewResponseService(opts ...option.RequestOption) (r *ResponseService) {
 func (r *ResponseService) Get(ctx context.Context, responseID string, query ResponseGetParams, opts ...option.RequestOption) (res *string, err error) {
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithHeader("Accept", "text/plain")}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.AccountID, precfg.AccountID)
 	if query.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -53,5 +58,7 @@ func (r *ResponseService) Get(ctx context.Context, responseID string, query Resp
 
 type ResponseGetParams struct {
 	// Account ID.
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
