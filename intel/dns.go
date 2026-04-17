30602758eab78e700aef1bbb95f43b3eac555725
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 09:53:25 -0700

    chore(intel): update generated types and methods

--- intel/dns.go
+++ intel/dns.go
@@ -43,6 +43,11 @@ func (r *DNSService) List(ctx context.Context, params DNSListParams, opts ...opt
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.AccountID, precfg.AccountID)
 	if params.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -125,6 +130,8 @@ func (r dnsReverseRecordJSON) RawJSON() string {
 
 type DNSListParams struct {
 	// Identifier.
+	//
+	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 	IPV4      param.Field[string] `query:"ipv4"`
 	// Requested page within paginated list of results.
