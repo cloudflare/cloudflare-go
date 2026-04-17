c1a30068ff03ba051fc28b4376ea5c2275043201
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 10:28:31 -0700

    chore: skip prism tests for magic_network_monitoring, secrets_store, stream
    These tests fail against the Prism mock server (422/415/406 errors).
    Skips cherry-picked from staging-next (5d9e6fc21).

--- secrets_store/store_test.go
+++ secrets_store/store_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 func TestStoreNew(t *testing.T) {
+	t.Skip("SKIP: prism error for 422 Unprocessable Entity")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
@@ -43,6 +44,7 @@ func TestStoreNew(t *testing.T) {
 }
 
 func TestStoreListWithOptionalParams(t *testing.T) {
+	t.Skip("SKIP: prism error for 422 Unprocessable Entity")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
@@ -72,6 +74,7 @@ func TestStoreListWithOptionalParams(t *testing.T) {
 }
 
 func TestStoreDelete(t *testing.T) {
+	t.Skip("SKIP: prism error for 422 Unprocessable Entity")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
--- stream/download_test.go
+++ stream/download_test.go
@@ -15,6 +15,7 @@ import (
 )
 
 func TestDownloadNew(t *testing.T) {
+	t.Skip("SKIP: prism 415 Unsupported Media Type")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
@@ -46,6 +47,7 @@ func TestDownloadNew(t *testing.T) {
 }
 
 func TestDownloadDelete(t *testing.T) {
+	t.Skip("SKIP: prism 415 Unsupported Media Type")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
@@ -76,6 +78,7 @@ func TestDownloadDelete(t *testing.T) {
 }
 
 func TestDownloadGet(t *testing.T) {
+	t.Skip("SKIP: prism 415 Unsupported Media Type")
 	baseURL := "http://localhost:4010"
 	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
 		baseURL = envURL
