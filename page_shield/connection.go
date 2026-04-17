79c747dddae90b3a267a98aeb3fc706800dd4efc
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 11:42:47 -0700

    chore(page_shield): update generated types and methods

--- page_shield/connection.go
+++ page_shield/connection.go
@@ -44,6 +44,11 @@ func (r *ConnectionService) List(ctx context.Context, params ConnectionListParam
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&params.ZoneID, precfg.ZoneID)
 	if params.ZoneID.Value == "" {
 		err = errors.New("missing required zone_id parameter")
 		return nil, err
@@ -70,6 +75,11 @@ func (r *ConnectionService) ListAutoPaging(ctx context.Context, params Connectio
 func (r *ConnectionService) Get(ctx context.Context, connectionID string, query ConnectionGetParams, opts ...option.RequestOption) (res *Connection, err error) {
 	var env ConnectionGetResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.ZoneID, precfg.ZoneID)
 	if query.ZoneID.Value == "" {
 		err = errors.New("missing required zone_id parameter")
 		return nil, err
@@ -134,6 +144,8 @@ func (r connectionJSON) RawJSON() string {
 
 type ConnectionListParams struct {
 	// Identifier
+	//
+	// Use [option.WithZoneID] on the client to set a global default for this field.
 	ZoneID param.Field[string] `path:"zone_id" api:"required"`
 	// The direction used to sort returned connections.
 	Direction param.Field[ConnectionListParamsDirection] `query:"direction"`
@@ -238,6 +250,8 @@ func (r ConnectionListParamsOrderBy) IsKnown() bool {
 
 type ConnectionGetParams struct {
 	// Identifier
+	//
+	// Use [option.WithZoneID] on the client to set a global default for this field.
 	ZoneID param.Field[string] `path:"zone_id" api:"required"`
 }
 
