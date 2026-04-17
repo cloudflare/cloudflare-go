0d2d07a36362802642c8f274201eb3e5b132a6be
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 10:04:53 -0700

    revert: exclude magic_network_monitoring, secrets_store, stream (breaking changes)

--- magic_network_monitoring/configfull.go
+++ magic_network_monitoring/configfull.go
@@ -39,11 +39,6 @@ func NewConfigFullService(opts ...option.RequestOption) (r *ConfigFullService) {
 func (r *ConfigFullService) Get(ctx context.Context, query ConfigFullGetParams, opts ...option.RequestOption) (res *Configuration, err error) {
 	var env ConfigFullGetResponseEnvelope
 	opts = slices.Concat(r.Options, opts)
-	precfg, err := requestconfig.PreRequestOptions(opts...)
-	if err != nil {
-		return nil, err
-	}
-	requestconfig.UseDefaultParam(&query.AccountID, precfg.AccountID)
 	if query.AccountID.Value == "" {
 		err = errors.New("missing required account_id parameter")
 		return nil, err
@@ -58,7 +53,6 @@ func (r *ConfigFullService) Get(ctx context.Context, query ConfigFullGetParams, 
 }
 
 type ConfigFullGetParams struct {
-	// Use [option.WithAccountID] on the client to set a global default for this field.
 	AccountID param.Field[string] `path:"account_id" api:"required"`
 }
 
