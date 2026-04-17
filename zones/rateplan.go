60efedebf8ba39e099664c1ea2b735511dc93cba
Vaishak Dinesh <vaishak@cloudflare.com>
2026-04-17 10:56:57 -0700

    chore(zones): update generated types and methods

--- zones/rateplan.go
+++ zones/rateplan.go
@@ -40,6 +40,11 @@ func (r *RatePlanService) Get(ctx context.Context, query RatePlanGetParams, opts
 	var raw *http.Response
 	opts = slices.Concat(r.Options, opts)
 	opts = append([]option.RequestOption{option.WithResponseInto(&raw)}, opts...)
+	precfg, err := requestconfig.PreRequestOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	requestconfig.UseDefaultParam(&query.ZoneID, precfg.ZoneID)
 	if query.ZoneID.Value == "" {
 		err = errors.New("missing required zone_id parameter")
 		return nil, err
@@ -165,5 +170,7 @@ func (r RatePlanGetResponseFrequency) IsKnown() bool {
 
 type RatePlanGetParams struct {
 	// Identifier
+	//
+	// Use [option.WithZoneID] on the client to set a global default for this field.
 	ZoneID param.Field[string] `path:"zone_id" api:"required"`
 }
